4c3dfd399d5536bb1e27f861980336d0e7f93bf1
tonic <tonicbupt@gmail.com>
2020-01-07 22:34:56 +0800

    remove vendor (#39)

--- vendor/github.com/ks3sdklib/aws-sdk-go/sdk.go
+++
@@ -1,7 +0,0 @@
-// Package sdk is the official AWS SDK for the Go programming language.
-//
-// See our Developer Guide for information for on getting started and using
-// the SDK.
-//
-// https://github.com/ks3sdklib/aws-sdk-go/wiki
-package sdk
--- vendor/qiniupkg.com/x/doc.go
+++
@@ -1,9 +0,0 @@
-package x
-
-import (
-	_ "qiniupkg.com/x/bytes.v7"
-	_ "qiniupkg.com/x/ctype.v7"
-	_ "qiniupkg.com/x/rpc.v7"
-	_ "qiniupkg.com/x/url.v7"
-)
-
