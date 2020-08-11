062e146a5362f519d2f296e8c1488d855413ecf0
chnliyong <4197225+chnliyong@users.noreply.github.com>
2020-08-11 21:39:08 +0800

    httpClient support proxy settings from environment variables (#67)
    * httpClient support proxy settings from environment variables
    
    * apply gofmt
    
    Co-authored-by: chnliyong <liyong@juicedata.io>

--- object/qiniu.go
+++ object/qiniu.go
@@ -134,7 +134,7 @@ func (q *qiniu) List(prefix, marker string, limit int64) ([]*Object, error) {
 			objs = append(objs, &Object{
 				entry.Key,
 				entry.Fsize,
-				time.Unix(0, entry.PutTime * 100),
+				time.Unix(0, entry.PutTime*100),
 				strings.HasSuffix(entry.Key, "/"),
 			})
 		}
