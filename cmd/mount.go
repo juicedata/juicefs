d0aca94df9770df571c39c4086d22eb669ba0606
Davies Liu <davies@juicedata.io>
2021-01-12 22:34:33 +0800

    compact data

--- cmd/mount.go
+++ cmd/mount.go
@@ -16,6 +16,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	_ "net/http/pprof"
@@ -149,6 +150,11 @@ func mount(c *cli.Context) error {
 		length := args[1].(uint32)
 		return store.Remove(chunkid, int(length))
 	}))
+	m.OnMsg(meta.CompactChunk, meta.MsgCallback(func(args ...interface{}) error {
+		chunkid := args[0].(uint64)
+		slices := args[1].([]meta.Slice)
+		return compact(chunkConf, store, slices, chunkid)
+	}))
 
 	conf := &vfs.Config{
 		Meta: &meta.Config{
@@ -280,4 +286,62 @@ func mountFlags() *cli.Command {
 			},
 		},
 	}
+}
+
+func readSlice(store chunk.ChunkStore, s *meta.Slice, page *chunk.Page, off int) error {
+	buf := page.Data
+	read := 0
+	reader := store.NewReader(s.Chunkid, int(s.Size))
+	for read < len(buf) {
+		p := page.Slice(read, len(buf)-read)
+		n, err := reader.ReadAt(context.Background(), p, off+int(s.Off))
+		p.Release()
+		if n == 0 && err != nil {
+			return err
+		}
+		read += n
+		off += n
+	}
+	return nil
+}
+
+func compact(conf chunk.Config, store chunk.ChunkStore, slices []meta.Slice, chunkid uint64) error {
+	writer := store.NewWriter(chunkid)
+	defer writer.Abort()
+
+	var pos int
+	for i, s := range slices {
+		if s.Chunkid == 0 {
+			_, err := writer.WriteAt(make([]byte, int(s.Len)), int64(pos))
+			if err != nil {
+				return err
+			}
+			pos += int(s.Len)
+			continue
+		}
+		var read int
+		for read < int(s.Len) {
+			l := utils.Min(conf.BlockSize, int(s.Len)-read)
+			p := chunk.NewOffPage(l)
+			if err := readSlice(store, &s, p, read); err != nil {
+				logger.Infof("can't compact chunk %d, retry later, read %d: %s", chunkid, i, err)
+				p.Release()
+				return err
+			}
+			_, err := writer.WriteAt(p.Data, int64(pos+read))
+			p.Release()
+			if err != nil {
+				logger.Errorf("can't compact chunk %d, retry later, write: %s", chunkid, err)
+				return err
+			}
+			read += l
+			if pos+read >= conf.BlockSize {
+				if err = writer.FlushTo(pos + read); err != nil {
+					panic(err)
+				}
+			}
+		}
+		pos += int(s.Len)
+	}
+	return writer.Finish(pos)
 }
