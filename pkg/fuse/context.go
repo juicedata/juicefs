35b1cd2adfd22f5602c70fe2a887f5b51e66b0e9
Davies Liu <davies@juicedata.io>
2021-01-25 20:49:53 +0800

    disable operation interrupt

--- pkg/fuse/context.go
+++ pkg/fuse/context.go
@@ -95,6 +95,6 @@ func (c *fuseContext) Err() error {
 	return syscall.EINTR
 }
 
-func (c *fuseContext) Done() <-chan struct{} {
-	return c.cancel
-}
+// func (c *fuseContext) Done() <-chan struct{} {
+// 	return c.cancel
+// }
