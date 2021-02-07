d4ebd2b493ff9ad7d6a82d3c256f4a39181a1736
Davies Liu <davies@juicedata.io>
2021-02-07 22:12:48 +0800

    Connect to Redis with Senitel

--- pkg/meta/redis.go
+++ pkg/meta/redis.go
@@ -23,6 +23,7 @@ import (
 	"fmt"
 	"hash/fnv"
 	"math/rand"
+	"net"
 	"os"
 	"strconv"
 	"strings"
@@ -114,13 +115,46 @@ func NewRedisMeta(url string, conf *RedisConfig) (Meta, error) {
 	if err != nil {
 		return nil, fmt.Errorf("parse %s: %s", url, err)
 	}
-	if opt.Password == "" && os.Getenv("REDIS_PASSWORD") != "" {
-		opt.Password = os.Getenv("REDIS_PASSWORD")
+	var rdb *redis.Client
+	if strings.Contains(opt.Addr, ",") {
+		var fopt redis.FailoverOptions
+		ps := strings.Split(opt.Addr, ",")
+		fopt.MasterName = ps[0]
+		fopt.SentinelAddrs = ps[1:]
+		_, port, _ := net.SplitHostPort(fopt.SentinelAddrs[len(fopt.SentinelAddrs)-1])
+		if port != "" {
+			for i := range fopt.SentinelAddrs {
+				h, p, _ := net.SplitHostPort(fopt.SentinelAddrs[i])
+				if p == "" {
+					fopt.SentinelAddrs[i] = net.JoinHostPort(h, port)
+				}
+			}
+		}
+		// Assume Redis server and sentinel have the same password.
+		fopt.SentinelPassword = opt.Password
+		fopt.Username = opt.Username
+		fopt.Password = opt.Password
+		if fopt.SentinelPassword == "" && os.Getenv("SENTINEL_PASSWORD") != "" {
+			fopt.SentinelPassword = os.Getenv("SENTINEL_PASSWORD")
+		}
+		if fopt.Password == "" && os.Getenv("REDIS_PASSWORD") != "" {
+			fopt.Password = os.Getenv("REDIS_PASSWORD")
+		}
+		fopt.DB = opt.DB
+		fopt.TLSConfig = opt.TLSConfig
+		fopt.MaxRetries = conf.Retries
+		fopt.MinRetryBackoff = time.Millisecond * 100
+		fopt.MaxRetryBackoff = time.Minute * 1
+		rdb = redis.NewFailoverClient(&fopt)
+	} else {
+		if opt.Password == "" && os.Getenv("REDIS_PASSWORD") != "" {
+			opt.Password = os.Getenv("REDIS_PASSWORD")
+		}
+		opt.MaxRetries = conf.Retries
+		opt.MinRetryBackoff = time.Millisecond * 100
+		opt.MaxRetryBackoff = time.Minute * 1
+		rdb = redis.NewClient(opt)
 	}
-	opt.MaxRetries = conf.Retries
-	opt.MinRetryBackoff = time.Millisecond * 100
-	opt.MaxRetryBackoff = time.Minute * 1
-	rdb := redis.NewClient(opt)
 	m := &redisMeta{
 		conf:         conf,
 		rdb:          rdb,
