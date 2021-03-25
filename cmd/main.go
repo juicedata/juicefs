d0156968b95f8d1978f1e54c0daad0d9a19768b5
Davies Liu <davies@juicedata.io>
2021-03-25 14:44:17 +0800

    re-order the arguments

--- cmd/main.go
+++ cmd/main.go
@@ -79,7 +79,7 @@ func main() {
 		}
 	}
 
-	err := app.Run(os.Args)
+	err := app.Run(reorderOptions(app, os.Args))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -158,6 +158,74 @@ func stringContains(s []string, e string) bool {
 		}
 	}
 	return false
+}
+
+func isFlag(flags []cli.Flag, option string) (bool, bool) {
+	if !strings.HasPrefix(option, "-") {
+		return false, false
+	}
+	// --V or -v work the same
+	option = strings.TrimLeft(option, "-")
+	for _, flag := range flags {
+		_, isBool := flag.(*cli.BoolFlag)
+		for _, name := range flag.Names() {
+			if option == name || strings.HasPrefix(option, name+"=") {
+				return true, !isBool && !strings.Contains(option, "=")
+			}
+		}
+	}
+	return false, false
+}
+
+func reorderOptions(app *cli.App, args []string) []string {
+	var newArgs = []string{args[0]}
+	var others []string
+	globalFlags := append(app.Flags, cli.VersionFlag)
+	for i := 1; i < len(args); i++ {
+		option := args[i]
+		if ok, hasValue := isFlag(globalFlags, option); ok {
+			newArgs = append(newArgs, option)
+			if hasValue {
+				i++
+				newArgs = append(newArgs, args[i])
+			}
+		} else {
+			others = append(others, option)
+		}
+	}
+	// no command
+	if len(others) == 0 {
+		return newArgs
+	}
+	cmdName := others[0]
+	var cmd *cli.Command
+	for _, c := range app.Commands {
+		if c.Name == cmdName {
+			cmd = c
+		}
+	}
+	if cmd == nil {
+		// can't recognize the command, skip it
+		return append(newArgs, others...)
+	}
+
+	newArgs = append(newArgs, cmdName)
+	args, others = others[1:], nil
+	// -h is valid for all the commands
+	cmdFlags := append(cmd.Flags, cli.HelpFlag)
+	for i := 0; i < len(args); i++ {
+		option := args[i]
+		if ok, hasValue := isFlag(cmdFlags, option); ok {
+			newArgs = append(newArgs, option)
+			if hasValue {
+				i++
+				newArgs = append(newArgs, args[i])
+			}
+		} else {
+			others = append(others, option)
+		}
+	}
+	return append(newArgs, others...)
 }
 
 func setLoggerLevel(c *cli.Context) {
