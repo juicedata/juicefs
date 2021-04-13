d65d2238b008bc8df11f4e54a43e7ddd973377e7
satoru <satorulogic@gmail.com>
2021-04-13 20:54:08 +0800

    Store Lua script in a separate file
    So that we can have editor support when writing embeded Lua scripts.

--- pkg/meta/redis.go
+++ pkg/meta/redis.go
@@ -18,6 +18,7 @@ package meta
 import (
 	"bytes"
 	"context"
+	_ "embed"
 	"encoding/binary"
 	"encoding/json"
 	"fmt"
@@ -63,29 +64,8 @@ const totalInodes = "totalInodes"
 const delfiles = "delfiles"
 const allSessions = "sessions"
 
-const scriptLookup = `
-local parse = function(buf, idx, pos)
-	return bit.lshift(string.byte(buf, idx), pos)
-end
-
-local buf = redis.call('HGET', KEYS[1], KEYS[2])
-if not buf then
-       return false
-end
-if string.len(buf) ~= 9 then
-       return {err=string.format("Invalid entry data: %s", buf)}
-end
-buf = string.sub(buf, 2)
-local ino =  parse(buf, 1, 56) +
-             parse(buf, 2, 48) +
-             parse(buf, 3, 40) +
-             parse(buf, 4, 32) +
-             parse(buf, 5, 24) +
-             parse(buf, 6, 16) +
-             parse(buf, 7, 8) +
-             parse(buf, 8, 0)
-return {ino, redis.call('GET', "i" .. tostring(ino))}
-`
+//go:embed lookup.lua
+var scriptLookup string
 
 // RedisConfig is config for Redis client.
 type RedisConfig struct {
