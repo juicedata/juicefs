3dea188d0cc92a0e3c99e88ba5de5cbf1a533af5
Davies Liu <davies@juicedata.io>
2021-05-12 09:37:14 +0800

    Speed up path resolution for Java SDK and S3 gateway using Lua script (#394)
    * Add Resolve method to the Meta interface
    
    * Check for success returned errno
    
    * Add function for parsing inode attr
    
    * Add function lookup
    
    * Add function for checking accessibility
    
    * Add functions for packing attr and resolving path
    
    * Avoid unnecessary attr unpacking
    
    * pass unit tests
    
    * add benchmark
    
    Co-authored-by: suzaku <satorulogic@gmail.com>

---
+++ pkg/meta/lua_scripts.go
@@ -0,0 +1,86 @@
+/*
+ * JuiceFS, Copyright (C) 2020 Juicedata, Inc.
+ *
+ * This program is free software: you can use, redistribute, and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3
+ * or later ("AGPL"), as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//nolint
+package meta
+
+const scriptLookup = `
+local buf = redis.call('HGET', KEYS[1], KEYS[2])
+if not buf then
+       return false
+end
+local ino = struct.unpack(">I8", string.sub(buf, 2))
+return {ino, redis.call('GET', "i" .. tostring(ino))}
+`
+
+const scriptResolve = `
+local function unpack_attr(buf)
+    local x = {}
+    x.flags, x.mode, x.uid, x.gid = struct.unpack(">BHI4I4", string.sub(buf, 0, 11))
+    x.type = (x.mode / 4096) % 8
+    x.mode = x.mode % 4096
+    return x
+end
+
+local function get_attr(ino)
+    local encoded_attr = redis.call('GET', "i" .. tostring(ino))
+    if not encoded_attr then
+        error("ENOENT")
+    end
+    return unpack_attr(encoded_attr)
+end
+
+local function lookup(parent, name)
+    local buf = redis.call('HGET', "d" .. tostring(parent), name)
+    if not buf then
+        error("ENOENT")
+    end
+    return struct.unpack(">BI8", buf)
+end
+
+local function can_access(ino, uid, gid)
+    if uid == 0 then
+        return true
+    end
+
+    local attr = get_attr(ino)
+    local mode = 0
+    if attr.uid == uid then
+        mode = (attr.mode / 64) % 8
+    elseif attr.gid == gid then
+        mode = (attr.mode / 8) % 8
+    else
+        mode = attr.mode % 8
+    end
+    return mode % 2 == 1
+end
+
+local function resolve(parent, path, uid, gid)
+    local _type = 2
+    for name in string.gmatch(path, "[^/]+") do
+        if _type == 3 then
+            error("ENOTSUP")
+        elseif _type ~= 2 then
+            error("ENOTDIR")
+        elseif parent > 1 and not can_access(parent, uid, gid) then 
+            error("EACCESS")
+        end
+        _type, parent = lookup(parent, name)
+    end
+    return {parent, redis.call('GET', "i" .. tostring(parent))}
+end
+
+return resolve(tonumber(KEYS[1]), KEYS[2], tonumber(KEYS[3]), tonumber(KEYS[4]))
+`
