998635eec0b8529eb92b867bde239c6b891ccb17
Davies Liu <davies@juicedata.io>
2021-06-04 14:10:31 +0800

    show a message when the mount point is ready

--- cmd/mount.go
+++ cmd/mount.go
@@ -195,6 +195,8 @@ func mount(c *cli.Context) error {
 		if err != nil {
 			logger.Fatalf("Failed to make daemon: %s", err)
 		}
+	} else {
+		go checkMountpoint(conf.Format.Name, mp)
 	}
 
 	installHandler(mp)
