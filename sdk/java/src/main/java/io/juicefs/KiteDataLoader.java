a35f7ce4668e3c210e46c8ffd091f5d662ff9fec
Davies Liu <davies@juicedata.io>
2021-11-03 12:20:28 +0800

    Update KiteDataLoader.java

--- sdk/java/src/main/java/io/juicefs/KiteDataLoader.java
+++ sdk/java/src/main/java/io/juicefs/KiteDataLoader.java
@@ -1,5 +1,5 @@
 /*
- * JuiceFS, Copyright (C) 2020 Juicedata, Inc.
+ * JuiceFS, Copyright (C) 2021 Juicedata, Inc.
  *
  * This program is free software: you can use, redistribute, and/or modify
  * it under the terms of the GNU Affero General Public License, version 3
