11ca69259b92a6ffbfcdb349069d4dd27a35f40f
Sandy Xu <sandy@juicedata.io>
2021-11-18 22:10:24 +0800

    meta/sql: fix delete condition (#1018)

--- pkg/meta/sql.go
+++ pkg/meta/sql.go
@@ -1642,7 +1642,7 @@ func (m *dbMeta) Rename(ctx Context, parentSrc Ino, nameSrc string, parentDst In
 						}
 						newSpace, newInode = -align4K(0), -1
 					}
-					if _, err := s.Delete(xattr{Inode: dino}); err != nil {
+					if _, err := s.Delete(&xattr{Inode: dino}); err != nil {
 						return err
 					}
 				}
@@ -2288,6 +2288,7 @@ func (m *dbMeta) deleteChunk(inode Ino, indx uint32) error {
 				return err
 			}
 		}
+		c.Slices = nil
 		n, err := ses.Where("inode = ? AND indx = ?", inode, indx).Delete(&c)
 		if err == nil && n == 0 {
 			err = fmt.Errorf("chunk %d:%d changed, try restarting transaction", inode, indx)
@@ -2323,7 +2324,7 @@ func (m *dbMeta) deleteFile(inode Ino, length uint64) {
 	for _, indx := range indexes {
 		err = m.deleteChunk(inode, indx)
 		if err != nil {
-			logger.Debugf("deleteChunk inode %d index %d error: %s", inode, indx, err)
+			logger.Warnf("deleteChunk inode %d index %d error: %s", inode, indx, err)
 			return
 		}
 	}
