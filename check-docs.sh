2f4f9072202fe14b9ec0733e54569730cb9c7096
sanwan <sanwan@juicedata.io>
2021-11-20 23:56:31 +0800

    test env CI

--- check-docs.sh
+++ check-docs.sh
@@ -5,7 +5,7 @@ set -e
 CHANGED_FILES=`git diff --name-only main...${TRAVIS_COMMIT}`
 echo $CHANGED_FILES
 DOCS_DIR="docs/.*"
-
+SKIP_FLAG=True
 echo "before CI"
 echo $CI
 
@@ -13,10 +13,15 @@ for CHANGED_FILE in $CHANGED_FILES; do
   echo "change files"
   echo $CHANGED_FILE
   if ! [[ $CHANGED_FILE =~ $DOCS_DIR ]] ; then
-    CI=false
+    SKIP_FLAG=False
     break
   fi
 done
 
+if [[ $SKIP_FLAG == True ]]; then
+  CI=false
+fi
+
+CI=false
 echo "after CI"
 echo ${CI}
