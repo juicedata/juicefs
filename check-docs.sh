9b27dd68ff3501757e1f274c320a9ef283d3cd08
sanwan <sanwan@juicedata.io>
2021-11-20 22:20:58 +0800

    add name only

--- check-docs.sh
+++ check-docs.sh
@@ -2,19 +2,23 @@
 
 set -e
 
-CHANGED_FILES=`git diff main...${TRAVIS_COMMIT}`
+CHANGED_FILES=`git diff --name-only main...${TRAVIS_COMMIT}`
 echo $CHANGED_FILES
-ONLY_DOCS=True
-DOC_DIR="docs/.*"
+SKIP_FLAG=True
+DOC_MD=".md"
+PIC_JPG=".jpg"
+PIC_png=".png"
+PIC_SVG=".svg"
 
 for CHANGED_FILE in $CHANGED_FILES; do
-  if ! [[ $CHANGED_FILE =~ $DOC_DIR ]]; then
-    ONLY_DOCS=False
+  if ![[ $CHANGED_FILE =~ $DOC_DIR ]] && ![[ $CHANGED_FILE =~ $PIC_JPG ]]  &&  \
+    ![[ $CHANGED_FILE =~ $PIC_PNG ]] && ![[ $CHANGED_FILE =~ $PIC_SVG ]] ; then
+    SKIP_FLAG=False
     break
   fi
 done
 
-if [[ $ONLY_DOCS == True ]]; then
+if [[ $SKIP_FLAG == True ]]; then
   echo "Only doc files modified, exiting."
   travis_terminate 0
   exit 1
