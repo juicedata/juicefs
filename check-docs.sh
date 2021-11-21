c60c33d37fe5ae4483251f2953b0b34128907b9d
sanwan <sanwan@juicedata.io>
2021-11-21 21:33:27 +0800

    self define env

--- check-docs.sh
+++ check-docs.sh
@@ -7,7 +7,7 @@ echo $CHANGED_FILES
 DOCS_DIR="docs/.*"
 SKIP_FLAG=True
 echo "before CI"
-echo $TRAVIS
+echo $CONTINUE_RUN
 
 for CHANGED_FILE in $CHANGED_FILES; do
   echo "change files"
@@ -19,9 +19,9 @@ for CHANGED_FILE in $CHANGED_FILES; do
 done
 
 if [[ $SKIP_FLAG == True ]]; then
-  TRAVIS=false
+  CONTINUE_RUN=false
 fi
 
-TRAVIS = true
+CONTINUE_RUN = true
 echo "after CI"
-echo ${TRAVIS}
+echo ${CONTINUE_RUN}
