8f741ee75495229392ea9c8c30c6a18e7e82f912
sanwan <sanwan@juicedata.io>
2021-11-22 16:48:39 +0800

    set TRAVIS true

--- check-docs.sh
+++ check-docs.sh
@@ -7,7 +7,7 @@ echo $TRAVIS_COMMIT
 CHANGED_FILES=`git diff --name-only main...${TRAVIS_COMMIT}`
 echo $CHANGED_FILES
 DOCS_DIR="docs/.*"
-SKIP_FLAG=True
+SKIP_FLAG=true
 echo "before CI"
 echo $TRAVIS
 echo "env list"
@@ -17,18 +17,16 @@ for CHANGED_FILE in $CHANGED_FILES; do
   echo "change files"
   echo $CHANGED_FILE
   if ! [[ $CHANGED_FILE =~ $DOCS_DIR ]] ; then
-    SKIP_FLAG=False
+    SKIP_FLAG=false
     break
   fi
 done
 
-if [[ $SKIP_FLAG == True ]]; then
+if [[ $SKIP_FLAG == true ]]; then
   TRAVIS=false
 fi
 
 
-
-
-TRAVIS=false
+TRAVIS=true
 echo "after CI"
 echo $TRAVIS
