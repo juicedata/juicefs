68141afdd3c7e1626530bb6de0698af06b6c3d5f
炽天 <hanxin.hx@alibaba-inc.com>
2021-11-22 16:16:58 +0800

    metadata: reset attr.Full to avoid mismatch between inode and attr
    If an "attr" is given with "Full=true", even if they do not match, the
    "GetAttr()" call will still be skipped.
    
    Signed-off-by: 炽天 <hanxin.hx@alibaba-inc.com>

--- pkg/meta/redis.go
+++ pkg/meta/redis.go
@@ -1934,6 +1934,10 @@ func (r *redisMeta) Open(ctx Context, inode Ino, flags uint32, attr *Attr) sysca
 	if r.conf.ReadOnly && flags&(syscall.O_WRONLY|syscall.O_RDWR|syscall.O_TRUNC|syscall.O_APPEND) != 0 {
 		return syscall.EROFS
 	}
+	if attr != nil {
+		// reset attr.Full to avoid mismatch between inode and attr
+		attr.Full = false
+	}
 	if r.conf.OpenCache > 0 && r.of.OpenCheck(inode, attr) {
 		return 0
 	}
