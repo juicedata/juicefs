b73c08cda2aa295c92cb79492bdce31d6636e924
sanwan <sanwan@juicedata.io>
2021-11-22 22:42:34 +0800

    delete debug info

--- check-docs.sh
+++ check-docs.sh
@@ -7,12 +7,6 @@ echo $CHANGED_FILES
 DOCS_DIR="docs/"
 SKIP_FLAG=true
 
-echo "TRAVIS_COMMIT_RANGE"
-echo $TRAVIS_COMMIT_RANGE
-echo "HEAD~1"
-git diff --name-only HEAD~1
-echo "TRAVIS_COMMIT_RANGE"
-git diff --name-only $TRAVIS_COMMIT_RANGE
 
 function changeFlag() {
   for CHANGED_FILE in $1; do
@@ -31,13 +25,6 @@ fi
 
 changeFlag CHANGED_FILES
 
-
-echo "skip flag"
-echo $SKIP_FLAG
-SKIP_FLAG=true
 if [[ $SKIP_FLAG == true ]]; then
   TRAVIS=false
 fi
-
-echo "TRAVIS"
-echo $TRAVIS
