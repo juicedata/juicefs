37c43647ad9195fab7295890f88d24b15ba1a1e1
sanwan <sanwan@juicedata.io>
2021-11-22 17:01:14 +0800

    delete unnecessary code

--- check-docs.sh
+++ check-docs.sh
@@ -2,20 +2,13 @@
 
 set -e
 
-echo "TRAVIS_COMMIT"
-echo $TRAVIS_COMMIT
 CHANGED_FILES=`git diff --name-only main...${TRAVIS_COMMIT}`
 echo $CHANGED_FILES
 DOCS_DIR="docs/.*"
 SKIP_FLAG=true
-echo "before CI"
-echo $TRAVIS
-echo "env list"
-env
+
 
 for CHANGED_FILE in $CHANGED_FILES; do
-  echo "change files"
-  echo $CHANGED_FILE
   if ! [[ $CHANGED_FILE =~ $DOCS_DIR ]] ; then
     SKIP_FLAG=false
     break
@@ -25,8 +18,3 @@ done
 if [[ $SKIP_FLAG == true ]]; then
   TRAVIS=false
 fi
-
-
-TRAVIS=true
-echo "after CI"
-echo $TRAVIS
