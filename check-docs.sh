d184f57a056a508e9c9a26ecee97ba8b52caeb95
sanwan <sanwan@juicedata.io>
2021-11-22 17:56:35 +0800

    test commit range

--- check-docs.sh
+++ check-docs.sh
@@ -6,9 +6,15 @@ echo "travis branch"
 echo $TRAVIS_BRANCH
 CHANGED_FILES=`git diff --name-only main...${TRAVIS_BRANCH}`
 echo $CHANGED_FILES
-DOCS_DIR="docs/.*"
+DOCS_DIR="docs/"
 SKIP_FLAG=true
 
+echo "TRAVIS_COMMIT_RANGE"
+echo $TRAVIS_COMMIT_RANGE
+echo "HEAD~1"
+git diff --name-only HEAD~1
+echo "TRAVIS_COMMIT_RANGE"
+git diff --name-only $TRAVIS_COMMIT_RANGE
 
 
 for CHANGED_FILE in $CHANGED_FILES; do
@@ -18,6 +24,11 @@ for CHANGED_FILE in $CHANGED_FILES; do
   fi
 done
 
+echo "skip flag"
+echo $SKIP_FLAG
 if [[ $SKIP_FLAG == true ]]; then
   TRAVIS=false
 fi
+
+echo "TRAVIS"
+echo $TRAVIS
