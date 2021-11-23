f593dbfc92398de6a9d5c915ddc173dfd3e5e0da
sanwan <sanwan@juicedata.io>
2021-11-23 09:57:44 +0800

    Skip travis check for changes on docs (#1029)
    Co-authored-by: Davies Liu <davies@juicedata.io>

---
+++ check-changed.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+if [ x"${TRAVIS_COMMIT_RANGE}" == x ] ; then
+  CHANGED_FILES=`git diff --name-only HEAD~1`
+else
+  CHANGED_FILES=`git diff --name-only $TRAVIS_COMMIT_RANGE`
+fi
+echo $CHANGED_FILES
+DOCS_DIR="docs/"
+SKIP_TEST=true
+
+for CHANGED_FILE in CHANGED_FILES; do
+  if ! [[ $CHANGED_FILE =~ $DOCS_DIR ]] ; then
+    SKIP_TEST=false
+    break
+  fi
+done
