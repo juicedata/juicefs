22970bcc01f8c7d6a0378b00b46f9d858077e717
sanwan <sanwan@juicedata.io>
2021-11-25 15:09:04 +0800

    change regex

--- .github/workflows/rm_syscalls_list.sh
+++ .github/workflows/rm_syscalls_list.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-LIST="cat $1"
+LIST=`cat $1`
 
 echo $LIST
 echo "before modified"
 cat $2
 for LINE in $LIST; do
-      sudo sed -i "s/$LINE//g" $2
+      sudo sed -i "s/^$LINE.*//g" $2
 done
 
 echo "after modified"
