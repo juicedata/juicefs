7a48f27d2f1c5f53c35fbd9029ad7d2c8fa35e8c
Davies Liu <davies@juicedata.io>
2021-12-13 15:40:34 +0800

    Nlink=1 after deleted if writeback_cache

--- pkg/fuse/fuse_test.go
+++ pkg/fuse/fuse_test.go
@@ -112,7 +112,7 @@ func mount(url, mp string) {
 	conf.DirEntryTimeout = time.Second
 	conf.HideInternal = true
 	v := vfs.NewVFS(conf, m, store)
-	err = Serve(v, "allow_other,writeback_cache,debug,rw", true)
+	err = Serve(v, "allow_other,debug,rw", true)
 	if err != nil {
 		log.Fatalf("fuse server err: %s\n", err)
 	}
