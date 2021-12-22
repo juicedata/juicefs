ddf27e4ccfe900eb12b11b9e103de2673b99e2a3
zhijian <djx@juicedata.io>
2021-12-22 13:49:23 +0800

    hadoop version 2.6.5 => 2.8.5

--- travis-setup-hdfs.sh
+++ travis-setup-hdfs.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 set -ex
 sudo apt-get update
 sudo apt-get install openjdk-8-jdk -y
 
-HADOOP_VERSION="2.6.5"
+HADOOP_VERSION="2.8.5"
 wget https://archive.apache.org/dist/hadoop/common/hadoop-${HADOOP_VERSION}/hadoop-${HADOOP_VERSION}.tar.gz
 mkdir ~/app
 tar -zxvf hadoop-${HADOOP_VERSION}.tar.gz -C ~/app
@@ -13,21 +13,27 @@ sudo tee -a ~/.bashrc <<EOF
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export JRE_HOME=\${JAVA_HOME}/jre
 export CLASSPATH=.:\${JAVA_HOME}/lib:\${JRE_HOME}/lib
-export PATH=${PATH}:\${JAVA_HOME}/bin
+export PATH=\${PATH}:\${JAVA_HOME}/bin
 
-export HADOOP_HOME=${HOME}/app/hadoop-${HADOOP_VERSION}
+export HADOOP_HOME=~/app/hadoop-${HADOOP_VERSION}
+export HADOOP_CONF_DIR=\${HADOOP_HOME}/etc/hadoop
 export PATH=\$PATH:\${HADOOP_HOME}/bin:\${HADOOP_HOME}/sbin
 EOF
+
 source ~/.bashrc
+echo $HADOOP_HOME
+echo $HADOOP_CONF_DIR
+echo $PATH
 
 ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa -q
 cat ~/.ssh/id_rsa.pub  >> ~/.ssh/authorized_keys
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/authorized_keys
+echo "StrictHostKeyChecking no" >> ~/.ssh/config
 
-sed -i 's/${JAVA_HOME}/\/usr\/lib\/jvm\/java-8-openjdk-amd64/g' /root/app/hadoop-${HADOOP_VERSION}/etc/hadoop/hadoop-env.sh
+sed -i 's/${JAVA_HOME}/\/usr\/lib\/jvm\/java-8-openjdk-amd64/g' ~/app/hadoop-${HADOOP_VERSION}/etc/hadoop/hadoop-env.sh
 
-sudo tee /root/app/hadoop-${HADOOP_VERSION}/etc/hadoop/core-site.xml <<EOF
+sudo tee ~/app/hadoop-${HADOOP_VERSION}/etc/hadoop/core-site.xml <<EOF
     <configuration>
         <property>
             <name>fs.defaultFS</name>
@@ -36,12 +42,12 @@ sudo tee /root/app/hadoop-${HADOOP_VERSION}/etc/hadoop/core-site.xml <<EOF
 
         <property>
             <name>hadoop.tmp.dir</name>
-            <value>/home/hadoop/app/tmp</value>
+            <value>${HOME}/apps/tmp</value>
         </property>
     </configuration>
 EOF
 
-sudo tee /root/app/hadoop-${HADOOP_VERSION}/etc/hadoop/hdfs-site.xml <<EOF
+sudo tee ~/app/hadoop-${HADOOP_VERSION}/etc/hadoop/hdfs-site.xml <<EOF
     <configuration>
         <property>
             <name>dfs.replication</name>
@@ -50,5 +56,11 @@ sudo tee /root/app/hadoop-${HADOOP_VERSION}/etc/hadoop/hdfs-site.xml <<EOF
     </configuration>
 EOF
 
-hdfs namenode -format
-start-dfs.sh
+cd ~/app/hadoop-${HADOOP_VERSION}/bin
+./hdfs namenode -format
+cd ~/app/hadoop-${HADOOP_VERSION}/sbin
+./start-dfs.sh
+
+jps
+
+echo "hdfs started successfully"
