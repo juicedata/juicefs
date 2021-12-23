2c1d7c650c2ba62a6cb5fe66317044ae57474065
zhijian <djx@juicedata.io>
2021-12-23 12:24:36 +0800

    debug

--- travis-setup-hdfs.sh
+++ travis-setup-hdfs.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-set -ex
+#  JuiceFS, Copyright (C) 2021 Juicedata, Inc.
+#
+#  This program is free software: you can use, redistribute, and/or modify
+#  it under the terms of the GNU Affero General Public License, version 3
+#  or later ("AGPL"), as published by the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful, but WITHOUT
+#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+set -e
 sudo apt-get update
 sudo apt-get install openjdk-8-jdk -y
 
