917f554c2ec6342ae900b04ff7276313fa6534e8
zhijian <djx@juicedata.io>
2021-12-28 08:49:51 +0800

    test ci

--- travis-setup-hdfs.sh
+++ travis-setup-hdfs.sh
@@ -23,6 +23,7 @@ wget https://dlcdn.apache.org/hadoop/core/stable2/hadoop-2.10.1.tar.gz
 mkdir ~/app
 tar -zxf hadoop-${HADOOP_VERSION}.tar.gz -C ~/app
 
+
 sudo tee -a ~/.bashrc <<EOF
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export JRE_HOME=\${JAVA_HOME}/jre
