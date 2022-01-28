12a1b8e8cd74fe90e322e7dac2e78b2abf7d0aae
sanwan <sanwan@juicedata.io>
2022-01-28 14:51:41 +0800

    change minio version

--- cmd/gc_test.go
+++ cmd/gc_test.go
@@ -76,9 +76,9 @@ func TestGc(t *testing.T) {
 
 	WriteLeakedData(dataDir)
 	time.Sleep(time.Duration(65)*time.Second)
-	
-	gcArgs = []string{"", "gc", "--delete",metaUrl}
-	err = Main(gcArgs)
+
+	gcArgs := []string{"", "gc", "--delete",metaUrl}
+	err := Main(gcArgs)
 	if err != nil {
 		t.Fatalf("gc failed: %v", err)
 	}
