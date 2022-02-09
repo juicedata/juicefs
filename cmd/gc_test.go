7b5b5b178923719c24c4bc62c1bd2fe3a303fecc
sanwan <sanwan@juicedata.io>
2022-02-09 13:11:44 +0800

    add gc compact test case (#1387)

--- cmd/gc_test.go
+++ cmd/gc_test.go
@@ -26,7 +26,7 @@ import (
 	"time"
 )
 
-func WriteLeakedData(dataDir string) {
+func writeLeakedData(dataDir string) {
 	var templateContent = "aaaaaaaabbbbbbbb"
 	var writeContent strings.Builder
 	for i := 0; i < 64*1024; i++ {
@@ -35,15 +35,15 @@ func WriteLeakedData(dataDir string) {
 	ioutil.WriteFile(dataDir+"chunks/0/0/"+"123456789_0_1048576", []byte(writeContent.String()), 0644)
 }
 
-func CheckLeakedData(dataDir string) bool {
-	_,err := os.Stat(dataDir+"chunks/0/0/"+"123456789_0_1048576")
+func checkLeakedData(dataDir string) bool {
+	_, err := os.Stat(dataDir + "chunks/0/0/" + "123456789_0_1048576")
 	if err != nil {
 		return true
 	}
 	return false
 }
 
-func RemoveAllFiles(dataDir string) {
+func removeAllFiles(dataDir string) {
 	_, err := os.Stat(dataDir)
 	if err == nil {
 		files, err := ioutil.ReadDir(dataDir)
@@ -55,11 +55,54 @@ func RemoveAllFiles(dataDir string) {
 	}
 }
 
+func writeSmallBlocks(mountDir string) error {
+	file, err := os.OpenFile(
+		mountDir+"/"+"test.txt",
+		os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
+		0666,
+	)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	var templateContent = "aaaaaaaabbbbbbbb"
+	var writeContent strings.Builder
+	for i := 0; i < 256; i++ {
+		writeContent.Write([]byte(templateContent))
+	}
+	for k := 0; k < 64; k++ {
+		_, err := file.Write([]byte(writeContent.String()))
+		if err != nil {
+			return err
+		}
+		syncErr := file.Sync()
+		if syncErr != nil {
+			return syncErr
+		}
+	}
+
+	return nil
+}
+
+func getFileCount(dir string) int {
+	files, _ := ioutil.ReadDir(dir)
+	count := 0
+	for _, f := range files {
+		if f.IsDir() {
+			count += getFileCount(dir + "/" + f.Name())
+		} else {
+			count++
+		}
+	}
+
+	return count
+}
+
 func TestGc(t *testing.T) {
 	metaUrl := "redis://127.0.0.1:6379/10"
 	mountpoint := "/tmp/testDir"
 	dataDir := "/tmp/testMountDir/test/"
-	RemoveAllFiles(dataDir + "chunks/")
+	removeAllFiles(dataDir + "chunks/")
 	defer ResetRedis(metaUrl)
 	if err := MountTmp(metaUrl, mountpoint); err != nil {
 		t.Fatalf("mount failed: %v", err)
@@ -70,6 +113,28 @@ func TestGc(t *testing.T) {
 			t.Fatalf("umount failed: %v", err)
 		}
 	}(mountpoint)
+
+	err := writeSmallBlocks(mountpoint)
+	if err != nil {
+		t.Fatalf("write small blocks failed,err is %v", err)
+	}
+	beforeCompactFileNum := getFileCount(dataDir + "chunks/")
+	gcArgs := []string{
+		"",
+		"gc",
+		"--compact",
+		metaUrl,
+	}
+	err = Main(gcArgs)
+	if err != nil {
+		t.Fatalf("gc failed: %v", err)
+	}
+	afterCompactFileNum := getFileCount(dataDir + "chunks/")
+	t.Logf("beforeCompactFileNum is %d,afterCompactFileNum is %d", beforeCompactFileNum, afterCompactFileNum)
+	if beforeCompactFileNum <= afterCompactFileNum {
+		t.Fatalf("gc compact failed")
+	}
+
 	for i := 0; i < 10; i++ {
 		filename := fmt.Sprintf("%s/f%d.txt", mountpoint, i)
 		err := ioutil.WriteFile(filename, []byte("test"), 0644)
@@ -81,21 +146,21 @@ func TestGc(t *testing.T) {
 	strEnvSkippedTime := os.Getenv("JFS_GC_SKIPPEDTIME")
 	t.Logf("JFS_GC_SKIPPEDTIME is %s", strEnvSkippedTime)
 
-	WriteLeakedData(dataDir)
+	writeLeakedData(dataDir)
 	time.Sleep(time.Duration(3) * time.Second)
 
-	gcArgs := []string{
+	gcArgs = []string{
 		"",
 		"gc",
 		"--delete",
 		metaUrl,
 	}
-	err := Main(gcArgs)
+	err = Main(gcArgs)
 	if err != nil {
 		t.Fatalf("gc failed: %v", err)
 	}
 
-	bNotExist := CheckLeakedData(dataDir)
+	bNotExist := checkLeakedData(dataDir)
 	if bNotExist == false {
 		t.Fatalf("gc delete failed,leaked data was not deleted")
 	}
