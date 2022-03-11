9e8de01d792723683f8b834a1d8a7ad63506ceb2
Sandy Xu <sandy@juicedata.io>
2022-03-11 20:44:07 +0800

    meta: encrypt the secret-key and encrypt-key in setting (#1562)
    * meta: encrypt the secret-key and encrypt-key in setting
    
    * only decrypte keys when creating storage
    
    * add unit test

--- pkg/meta/sql.go
+++ pkg/meta/sql.go
@@ -257,7 +257,7 @@ func (m *dbMeta) Init(format Format, force bool) error {
 			return fmt.Errorf("json: %s", err)
 		}
 		if force {
-			old.SecretKey = "removed"
+			old.RemoveSecret()
 			logger.Warnf("Existing volume will be overwrited: %+v", old)
 		} else {
 			format.UUID = old.UUID
@@ -265,14 +265,16 @@ func (m *dbMeta) Init(format Format, force bool) error {
 			old.Bucket = format.Bucket
 			old.AccessKey = format.AccessKey
 			old.SecretKey = format.SecretKey
+			old.EncryptKey = format.EncryptKey
+			old.KeyEncrypted = format.KeyEncrypted
 			old.Capacity = format.Capacity
 			old.Inodes = format.Inodes
 			old.TrashDays = format.TrashDays
 			old.MinClientVersion = format.MinClientVersion
 			old.MaxClientVersion = format.MaxClientVersion
 			if format != old {
-				old.SecretKey = ""
-				format.SecretKey = ""
+				old.RemoveSecret()
+				format.RemoveSecret()
 				return fmt.Errorf("cannot update format from %+v to %+v", old, format)
 			}
 		}
