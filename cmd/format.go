4375d624fc841dade1743ac755e3af086363942a
Zhou Cheng <zhoucheng@juicedata.io>
2022-03-31 15:19:54 +0800

    upgrade meta version to 2 for testing

--- cmd/format.go
+++ cmd/format.go
@@ -330,7 +330,7 @@ func format(c *cli.Context) error {
 			BlockSize:   fixObjectSize(c.Int("block-size")),
 			Compression: c.String("compress"),
 			TrashDays:   c.Int("trash-days"),
-			MetaVersion: 1,
+			MetaVersion: 2,
 		}
 		if format.AccessKey == "" && os.Getenv("ACCESS_KEY") != "" {
 			format.AccessKey = os.Getenv("ACCESS_KEY")
