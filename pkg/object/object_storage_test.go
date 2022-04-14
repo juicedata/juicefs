d53361959dac501ce580c7383275e66089c1ea3e
zhijian <djx@juicedata.io>
2022-04-14 11:13:19 +0800

    implement list api for swift

--- pkg/object/object_storage_test.go
+++ pkg/object/object_storage_test.go
@@ -476,6 +476,17 @@ func TestTiKV(t *testing.T) {
 	testStorage(t, s)
 }
 
+func TestSwift(t *testing.T) {
+	if os.Getenv("SWIFT_ADDR") == "" {
+		t.SkipNow()
+	}
+	s, err := newSwiftOSS(os.Getenv("SWIFT_ADDR"), "", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	testStorage(t, s)
+}
+
 func TestWebDAV(t *testing.T) {
 	if os.Getenv("WEBDAV_TEST_BUCKET") == "" {
 		t.SkipNow()
