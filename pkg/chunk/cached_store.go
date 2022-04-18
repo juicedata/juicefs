eef307b28480f29a862a652fec5418caa98dab56
Sandy Xu <sandy@juicedata.io>
2022-04-18 13:40:07 +0800

    chunk/store: check pendingKeys again if open staging file failed (#1811)

--- pkg/chunk/cached_store.go
+++ pkg/chunk/cached_store.go
@@ -833,7 +833,14 @@ func (store *cachedStore) uploadStagingFile(key string, stagingPath string) {
 	}
 	f, err := os.Open(stagingPath)
 	if err != nil {
-		logger.Errorf("Open staging file %s: %s", stagingPath, err)
+		store.pendingMutex.Lock()
+		_, ok = store.pendingKeys[key]
+		store.pendingMutex.Unlock()
+		if ok {
+			logger.Errorf("Open staging file %s: %s", stagingPath, err)
+		} else {
+			logger.Debugf("Key %s is not needed, drop it", key)
+		}
 		return
 	}
 	blen := parseObjOrigSize(key)
