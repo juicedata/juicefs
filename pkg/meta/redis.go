436859d39db9699cd1d7d1421d2fbfa1fda62871
zhijian <djx@juicedata.io>
2022-05-20 08:54:42 +0800

    meta/redis: check config 'maxmemory-policy' (#2059)

--- pkg/meta/redis.go
+++ pkg/meta/redis.go
@@ -2769,11 +2769,21 @@ func (m *redisMeta) checkServerConfig() {
 		logger.Warnf("parse info: %s", err)
 		return
 	}
-	_, err = checkRedisInfo(rawInfo)
+	rInfo, err := checkRedisInfo(rawInfo)
 	if err != nil {
 		logger.Warnf("parse info: %s", err)
 	}
-
+	if rInfo.maxMemoryPolicy != "noeviction" {
+		if _, err := m.rdb.ConfigSet(Background, "maxmemory-policy", "noeviction").Result(); err != nil {
+			logger.Errorf("try to reconfigure maxmemory-policy to 'noeviction' failed: %s", err)
+		} else if result, err := m.rdb.ConfigGet(Background, "maxmemory-policy").Result(); err != nil {
+			logger.Warnf("get config maxmemory-policy failed: %s", err)
+		} else if len(result) == 2 && result[1] != "noeviction" {
+			logger.Warnf("reconfigured maxmemory-policy to 'noeviction', but it's still %s", result[1])
+		} else {
+			logger.Infof("set maxmemory-policy to 'noeviction' successfully")
+		}
+	}
 	start := time.Now()
 	_ = m.rdb.Ping(Background)
 	logger.Infof("Ping redis: %s", time.Since(start))
