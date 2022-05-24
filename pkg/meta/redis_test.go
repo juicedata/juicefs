f3c215998538cd46e4f68f82ca561bb77380cb69
Davies Liu <davies@juicedata.io>
2022-05-24 13:13:17 +0800

    meta/sql: fix mount with read-only (#2086)

--- pkg/meta/redis_test.go
+++ pkg/meta/redis_test.go
@@ -1291,6 +1291,9 @@ func testReadOnly(t *testing.T, m Meta) {
 
 	var inode Ino
 	var attr = &Attr{}
+	if st := m.GetAttr(ctx, 1, attr); st != 0 {
+		t.Fatalf("mkdir d: %s", st)
+	}
 	if st := m.Mkdir(ctx, 1, "d", 0640, 022, 0, &inode, attr); st != syscall.EROFS {
 		t.Fatalf("mkdir d: %s", st)
 	}
