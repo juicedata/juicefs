845215dc8222dc7a4958d00b87050cc9cfe97c24
Davies Liu <davies@juicedata.io>
2022-06-18 09:41:56 +0800

    fix concurrent rmr/warmup in mac (#2265)

--- pkg/vfs/internal.go
+++ pkg/vfs/internal.go
@@ -22,6 +22,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 	"sync/atomic"
 	"syscall"
 	"time"
@@ -40,6 +41,31 @@ const (
 	configInode     = minInternalNode + 4
 	trashInode      = meta.TrashInode
 )
+
+var controlMutex sync.Mutex
+var controlHandlers = make(map[uint32]uint64)
+
+func (v *VFS) getControlHandle(pid uint32) uint64 {
+	controlMutex.Lock()
+	defer controlMutex.Unlock()
+	fh := controlHandlers[pid]
+	if fh == 0 {
+		h := v.newHandle(controlInode)
+		fh = h.fh
+		controlHandlers[pid] = fh
+	}
+	return fh
+}
+
+func (v *VFS) releaseControlHandle(pid uint32) {
+	controlMutex.Lock()
+	defer controlMutex.Unlock()
+	fh := controlHandlers[pid]
+	if fh != 0 {
+		v.releaseHandle(controlInode, fh)
+		delete(controlHandlers, pid)
+	}
+}
 
 type internalNode struct {
 	inode Ino
