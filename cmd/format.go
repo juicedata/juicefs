97a43a70e3d1dc5bb05ade4fce365744b0f02a24
zhijian <djx@juicedata.io>
2022-06-27 15:28:49 +0800

    fix skip tls verify

--- cmd/format.go
+++ cmd/format.go
@@ -193,23 +193,23 @@ func createStorage(format meta.Format) (object.ObjectStorage, error) {
 	object.UserAgent = "JuiceFS-" + version.Version()
 	var blob object.ObjectStorage
 	var err error
-	var query string
-	if p := strings.Index(format.Bucket, "?"); p > 0 && p+1 < len(format.Bucket) {
-		query = format.Bucket[p+1:]
-		format.Bucket = format.Bucket[:p]
-		logger.Debugf("query string: %s", query)
+
+	u, err := url.Parse(format.Bucket)
+	if err != nil {
+		return nil, err
 	}
-	if query != "" {
-		values, err := url.ParseQuery(query)
-		if err != nil {
-			return nil, err
-		}
+	values := u.Query()
+	if len(values) != 0 {
 		var tlsSkipVerify bool
 		if tlsSkipVerify, err = strconv.ParseBool(values.Get("tls-insecure-skip-verify")); err != nil {
 			return nil, err
 		}
 		object.GetHttpClient().Transport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: tlsSkipVerify}
+		values.Del("tls-insecure-skip-verify")
 	}
+	u.RawQuery = values.Encode()
+	format.Bucket = u.String()
+
 	if format.Shards > 1 {
 		blob, err = object.NewSharded(strings.ToLower(format.Storage), format.Bucket, format.AccessKey, format.SecretKey, format.SessionToken, format.Shards)
 	} else {
