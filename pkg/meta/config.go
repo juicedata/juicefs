69d3f893321cd0b0a8d598dff3883c878c264767
Sandy Xu <sandy@juicedata.io>
2022-07-29 15:28:47 +0800

    meta: check metadata version after setting reloaded (#2420)

--- pkg/meta/config.go
+++ pkg/meta/config.go
@@ -115,7 +115,7 @@ func (f *Format) String() string {
 }
 
 func (f *Format) CheckVersion() error {
-	if f.MetaVersion > 1 {
+	if f.MetaVersion > MaxVersion {
 		return fmt.Errorf("incompatible metadata version: %d; please upgrade the client", f.MetaVersion)
 	}
 
