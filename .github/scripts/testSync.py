4cf2c0a1d14e3a1beb6a514048dddff11abb7094
Zhou Cheng <zhoucheng@juicedata.io>
2022-08-18 12:17:12 +0800

    add idempotent

--- .github/scripts/testSync.py
+++ .github/scripts/testSync.py
@@ -1,7 +1,6 @@
 import subprocess
 import random
 import shutil
-from warnings import catch_warnings
 from hypothesis import given, strategies as st, settings, example, assume
 from hypothesis.strategies import composite, tuples
 import os
@@ -10,10 +9,8 @@ entries = set()
 jfs_source_dir=os.path.expanduser('~/Documents/juicefs2/pkg/')
 jfs_source_dir='jfs_source/pkg/'
 
-
 def setup():
     meta_url = "sqlite3://abc.db"
-    # meta_url='redis://localhost/1'
     mount_point='/tmp/sync-test/'
     volume_name='sync-test'
     if os.path.isfile('abc.db'):
@@ -57,24 +54,47 @@ path_entry = st.lists(st.sampled_from(list(entries))).map(lambda x: change_entry
 valid_name = st.text(st.characters(max_codepoint=1000, blacklist_categories=('Cc', 'Cs')), min_size=2).map(lambda s: s.strip()).filter(lambda s: len(s) > 0)
 random_text = st.lists(valid_name).map(lambda x: change_entry(x))
 
+@given(sync_options=random_text)
+@settings(max_examples=10000, deadline=None)
+def test_sync_with_random_text(sync_options):
+    compare_rsync_and_juicesync(sync_options)
+
+@given(sync_options=path_entry)
+@settings(max_examples=10000, deadline=None)
+def test_sync_with_path_entry(sync_options):
+    compare_rsync_and_juicesync(sync_options)
+
 @given(options=random_text)
 @settings(max_examples=100, deadline=None)
-# @example([['--exclude', "'1Ǹ[*ǣ'"]])
-def test_sync_with_random_text(options):
-    # pass
-    # if sync_options == '--exclude *':
-    #     # rsync does not support
-    #     assume()
-    sync(options)
+def test_idempotent(sync_options):
+    assert len(sync_options) != 0
+    sync_options = [item for sublist in sync_options for item in sublist]
+    if os.path.exists('juicesync_dir1/'):
+        shutil.rmtree('juicesync_dir1/')
+    os.makedirs('juicesync_dir1')
+    if os.path.exists('juicesync_dir2/'):
+        shutil.rmtree('juicesync_dir2/')
+    os.makedirs('juicesync_dir2')
+    juicesync_cmd = ['./juicefs' , 'sync', '--dirs', jfs_source_dir,  'juicesync_dir1/']+sync_options
+    print('juicesync_cmd: '+' '.join(juicesync_cmd))
+    try:
+        subprocess.check_call(juicesync_cmd)
+    except Exception as e:
+        assert False
+    
+    juicesync_cmd = ['./juicefs' , 'sync', '--dirs',  'juicesync_dir1/', 'juicesync_dir2/']+sync_options
+    print('juicesync_cmd: '+' '.join(juicesync_cmd))
+    try:
+        subprocess.check_call(juicesync_cmd)
+    except Exception as e:
+        assert False
 
-@given(options=path_entry)
-@settings(max_examples=100, deadline=None)
-def test_sync_with_path_entry(options):
-    sync(options)
+    diff_result = os.system('diff -ur juicesync_dir1 juicesync_dir2')
+    assert diff_result==0
 
-def sync(sync_options):
-    if not sync_options:
-        return
+
+def compare_rsync_and_juicesync(sync_options):
+    assert len(sync_options) != 0
     sync_options = [item for sublist in sync_options for item in sublist]
     if os.path.exists('rsync_dir/'):
         shutil.rmtree('rsync_dir/')
