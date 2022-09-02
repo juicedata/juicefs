d039c2b72c7eac6e77eed7dd85511825f9fb0103
Zhou Cheng <zhoucheng@juicedata.io>
2022-09-02 21:25:50 +0800

    optimize mount and gateway test

--- .github/scripts/testVersionCompatible.py
+++ .github/scripts/testVersionCompatible.py
@@ -25,6 +25,7 @@ class JuicefsMachine(RuleBasedStateMachine):
     # META_URL = 'badger://abc.db'
     MOUNT_POINT = '/tmp/sync-test/'
     VOLUME_NAME = 'test-volume'
+    valid_file_name = st.text(st.characters(max_codepoint=1000, blacklist_categories=('Cc', 'Cs')), min_size=2).map(lambda s: s.strip()).filter(lambda s: len(s) > 0)
 
     def __init__(self):
         super(JuicefsMachine, self).__init__()
@@ -170,15 +171,95 @@ class JuicefsMachine(RuleBasedStateMachine):
             assert len(sessions) != 0 
         print('status succeed')
 
-    @rule(juicefs=st.sampled_from(JFS_BINS))
+    @rule(juicefs=st.sampled_from(JFS_BINS), 
+        no_syslog=st.booleans(),
+        other_fuse_options=st.lists(st.sampled_from(['debug', 'allow_other', 'writeback_cache']), unique=True), 
+        enable_xattr=st.booleans(),
+        attr_cache=st.integers(min_value=1, max_value=10), 
+        entry_cache=st.integers(min_value=1, max_value=10), 
+        dir_entry_cache=st.integers(min_value=1, max_value=10), 
+        get_timeout=st.integers(min_value=30, max_value=60), 
+        put_timeout=st.integers(min_value=30, max_value=60), 
+        io_retries=st.integers(min_value=5, max_value=15), 
+        max_uploads=st.integers(min_value=1, max_value=100), 
+        max_deletes=st.integers(min_value=1, max_value=100), 
+        buffer_size=st.integers(min_value=100, max_value=1000), 
+        upload_limit=st.integers(min_value=0, max_value=1000), 
+        download_limit=st.integers(min_value=0, max_value=1000), 
+        prefetch=st.integers(min_value=0, max_value=100), 
+        writeback=st.booleans(),
+        upload_delay=st.integers(min_value=0, max_value=60), 
+        cache_dir=valid_file_name,
+        cache_size=st.integers(min_value=0, max_value=1024000), 
+        free_space_ratio=st.floats(min_value=0.1, max_value=0.5), 
+        cache_partial_only=st.booleans(),
+        backup_meta=st.integers(min_value=300, max_value=3600),
+        heartbeat=st.integers(min_value=1, max_value=12), 
+        read_only=st.booleans(),
+        no_bgjob=st.booleans(),
+        open_cache=st.integers(min_value=0, max_value=100),
+        sub_dir=valid_file_name,
+        metrics=st.sampled_from(['127.0.0.1:9567', '127.0.0.1:9568']), 
+        consul=st.sampled_from(['127.0.0.1:8500', '127.0.0.1:8501']), 
+        no_usage_report=st.booleans(),
+    )
     @precondition(lambda self: self.formatted )
-    def mount(self, juicefs):
+    def mount(self, juicefs, no_syslog, other_fuse_options, enable_xattr, attr_cache, entry_cache, dir_entry_cache,
+        get_timeout, put_timeout, io_retries, max_uploads, max_deletes, buffer_size, upload_limit, download_limit, prefetch, 
+        writeback, upload_delay, cache_dir, cache_size, free_space_ratio, cache_partial_only, backup_meta, heartbeat, read_only,
+        no_bgjob, open_cache, sub_dir, metrics, consul, no_usage_report):
         assume (self.is_supported_version(juicefs))
         print('start mount')
-        run_jfs_cmd([juicefs, 'mount', '-d',  self.meta_url, JuicefsMachine.MOUNT_POINT])
+        options = [juicefs, 'mount', '-d',  self.meta_url, JuicefsMachine.MOUNT_POINT]
+        if no_syslog:
+            options.append('--no-syslog')
+        options.extend(['--log', os.path.expanduser(f'~/.juicefs/juicefs.log')])
+        if other_fuse_options:
+            options.extend(['-o', ','.join(other_fuse_options)])
+        if 'user_allow_other' in other_fuse_options:
+            if os.path.exists('/etc/fuse.conf'):
+                # subprocess.check_call(['sudo', 'bash',  '-c', '"echo user_allow_other >>/etc/fuse.conf"' ])
+                os.system('sudo bash -c "echo user_allow_other >>/etc/fuse.conf"')
+        if enable_xattr:
+            options.append('--enable-xattr')
+        options.extend(['--attr-cache', str(attr_cache)])
+        options.extend(['--entry-cache', str(entry_cache)])
+        options.extend(['--dir-entry-cache', str(dir_entry_cache)])
+        options.extend(['--get-timeout', str(get_timeout)])
+        options.extend(['--put-timeout', str(put_timeout)])
+        options.extend(['--io-retries', str(io_retries)])
+        options.extend(['--max-uploads', str(max_uploads)])
+        options.extend(['--max-deletes', str(max_deletes)])
+        options.extend(['--buffer-size', str(buffer_size)])
+        options.extend(['--upload-limit', str(upload_limit)])
+        options.extend(['--download-limit', str(download_limit)])
+        options.extend(['--prefetch', str(prefetch)])
+        if writeback:
+            options.append('--writeback')
+        options.extend(['--upload-delay', str(upload_delay)])
+        options.extend(['--cache-dir', os.path.expanduser(f'~/.juicefs/{cache_dir}')])
+        options.extend(['--cache-size', str(cache_size)])
+        options.extend(['--free-space-ratio', str(free_space_ratio)])
+        if cache_partial_only:
+            options.append('--cache-partial-only')
+        options.extend(['--backup-meta', str(backup_meta)])
+        options.extend(['--heartbeat', str(heartbeat)])
+        if read_only:
+            options.append('--read-only')
+        if no_bgjob:
+            options.append('--no-bgjob')
+
+        options.extend(['--open-cache', str(open_cache)])
+        print('TODO: subdir')
+        # options.extend('--subdir', str(sub_dir))
+        options.extend(['--metrics', str(metrics)])
+        options.extend(['--consul', str(consul)])
+        if no_usage_report:
+            options.append('--no-usage-report')
+        run_jfs_cmd(options)
         time.sleep(2)
         output = subprocess.check_output([juicefs, 'status', self.meta_url])
-        print(f'status output: {output.decode()}')
+        print(f'status output: {output}')
         sessions = json.loads(output.decode().replace("'", '"'))['Sessions']
         assert len(sessions) != 0 
         self.mounted = True
@@ -213,7 +294,6 @@ class JuicefsMachine(RuleBasedStateMachine):
         self.mounted = False
         print('destroy succeed')
 
-    valid_file_name = st.text(st.characters(max_codepoint=1000, blacklist_categories=('Cc', 'Cs')), min_size=2).map(lambda s: s.strip()).filter(lambda s: len(s) > 0)
     @rule(file_name=valid_file_name, data=st.binary() )
     @precondition(lambda self: self.mounted )
     def write_and_read(self, file_name, data):
@@ -338,6 +418,103 @@ class JuicefsMachine(RuleBasedStateMachine):
         run_jfs_cmd(options)
         # print(output)
         print('gc succeed')
+
+
+    valid_file_name = st.text(st.characters(max_codepoint=1000, blacklist_categories=('Cc', 'Cs')), min_size=2).map(lambda s: s.strip()).filter(lambda s: len(s) > 0)
+    @rule(juicefs=st.sampled_from(JFS_BINS), 
+        get_timeout=st.integers(min_value=30, max_value=60), 
+        put_timeout=st.integers(min_value=30, max_value=60), 
+        io_retries=st.integers(min_value=5, max_value=15), 
+        max_uploads=st.integers(min_value=1, max_value=100), 
+        max_deletes=st.integers(min_value=1, max_value=100), 
+        buffer_size=st.integers(min_value=100, max_value=1000), 
+        upload_limit=st.integers(min_value=0, max_value=1000), 
+        download_limit=st.integers(min_value=0, max_value=1000), 
+        prefetch=st.integers(min_value=0, max_value=100), 
+        writeback=st.booleans(),
+        upload_delay=st.integers(min_value=0, max_value=60), 
+        cache_dir=valid_file_name,
+        cache_size=st.integers(min_value=0, max_value=1024000), 
+        free_space_ratio=st.floats(min_value=0.1, max_value=0.5), 
+        cache_partial_only=st.booleans(),
+        backup_meta=st.integers(min_value=300, max_value=3600),
+        heartbeat=st.integers(min_value=1, max_value=12), 
+        read_only=st.booleans(),
+        no_bgjob=st.booleans(),
+        open_cache=st.integers(min_value=0, max_value=100),
+        attr_cache=st.integers(min_value=1, max_value=10), 
+        entry_cache=st.integers(min_value=1, max_value=10), 
+        dir_entry_cache=st.integers(min_value=1, max_value=10), 
+        access_log=valid_file_name,
+        no_banner=st.booleans(),
+        multi_buckets=st.booleans(), 
+        keep_etag=st.booleans(),
+        umask=st.sampled_from(['022', '755']), 
+        metrics=st.sampled_from(['127.0.0.1:9567', '127.0.0.1:9568']), 
+        consul=st.sampled_from(['127.0.0.1:8500', '127.0.0.1:8501']), 
+        no_usage_report=st.booleans(),
+        sub_dir=valid_file_name,
+        port=st.integers(min_value=9001, max_value=10000))
+    )
+    @precondition(lambda self: self.formatted )
+    def gateway(self, juicefs, get_timeout, put_timeout, io_retries, max_uploads, max_deletes, buffer_size, upload_limit, 
+        download_limit, prefetch, writeback, upload_delay, cache_dir, cache_size, free_space_ratio, cache_partial_only, 
+        backup_meta,heartbeat, read_only, no_bgjob, open_cache, attr_cache, entry_cache, dir_entry_cache, access_log, 
+        no_banner, multi_buckets, keep_etag, umask, metrics, consul, no_usage_report, sub_dir, port):
+        assume (self.is_supported_version(juicefs))
+        assume(self.is_port_in_use(port))
+        print('start gateway')
+        os.environ['MINIO_ROOT_USER'] = 'admin'
+        os.environ['MINIO_ROOT_PASSWORD'] = '12345678'
+        options = [juicefs, 'gateway', self.meta_url, f'localhost:{port}']
+        
+        options.extend(['--attr-cache', str(attr_cache)])
+        options.extend(['--entry-cache', str(entry_cache)])
+        options.extend(['--dir-entry-cache', str(dir_entry_cache)])
+        options.extend(['--get-timeout', str(get_timeout)])
+        options.extend(['--put-timeout', str(put_timeout)])
+        options.extend(['--io-retries', str(io_retries)])
+        options.extend(['--max-uploads', str(max_uploads)])
+        options.extend(['--max-deletes', str(max_deletes)])
+        options.extend(['--buffer-size', str(buffer_size)])
+        options.extend(['--upload-limit', str(upload_limit)])
+        options.extend(['--download-limit', str(download_limit)])
+        options.extend(['--prefetch', str(prefetch)])
+        if writeback:
+            options.append('--writeback')
+        options.extend(['--upload-delay', str(upload_delay)])
+        options.extend(['--cache-dir', os.path.expanduser(f'~/.juicefs/{cache_dir}')])
+        options.extend(['--access-log', os.path.expanduser(f'~/.juicefs/{access_log}')])
+        options.extend(['--cache-size', str(cache_size)])
+        options.extend(['--free-space-ratio', str(free_space_ratio)])
+        if cache_partial_only:
+            options.append('--cache-partial-only')
+        options.extend(['--backup-meta', str(backup_meta)])
+        options.extend(['--heartbeat', str(heartbeat)])
+        if read_only:
+            options.append('--read-only')
+        if no_bgjob:
+            options.append('--no-bgjob')
+        if no_banner:
+            options.append('--no-banner')
+        if multi_buckets:
+            options.append('--multi-buckets')
+        if keep_etag:
+            options.append('--keep-etag')
+        options.extend(['--umask', umask])
+
+        options.extend(['--open-cache', str(open_cache)])
+        print(f'TODO: subdir:{sub_dir}')
+        # options.extend('--subdir', str(sub_dir))
+        options.extend(['--metrics', str(metrics)])
+        options.extend(['--consul', str(consul)])
+        if no_usage_report:
+            options.append('--no-usage-report')
+
+        proc=subprocess.Popen(options)
+        time.sleep(2.0)
+        subprocess.Popen.kill(proc)
+        print('gateway succeed')
 
     @rule(juicefs = st.sampled_from(JFS_BINS),
         port=st.integers(min_value=9001, max_value=10000))
