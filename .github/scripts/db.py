eb2041799e11ad32ef210d821d92c2002d30b937
Zhou Cheng <zhoucheng@juicedata.io>
2022-10-13 14:03:48 +0800

    fix bug

--- .github/scripts/db.py
+++ .github/scripts/db.py
@@ -5,22 +5,37 @@ import MySQLdb
 from datetime import datetime
 import argparse
 
+# CREATE TABLE `benchmark` (
+#   `name` varchar(128) DEFAULT NULL,
+#   `result` float DEFAULT NULL,
+#   `product_name` varchar(32) DEFAULT NULL,
+#   `product_version` varchar(128) DEFAULT NULL,
+#   `meta` varchar(32) DEFAULT NULL,
+#   `storage` varchar(32) DEFAULT NULL,
+#   `github_ref_name` varchar(64) DEFAULT NULL,
+#   `github_run_id` varchar(1024) DEFAULT NULL,
+#   `github_sha` varchar(1024) DEFAULT NULL,
+#   `github_runner` varchar(255) DEFAULT NULL,
+#   `created_date` datetime DEFAULT NULL
+# )
+
 def add_perf_record(name, result, product_version,  meta, storage):
+    result = float(result)
     passowrd = os.environ['MYSQL_PASSWORD']
-    github_ref_name = os.environ['GITHUB_REF_NAME']
+    github_ref_name = os.environ.get('GITHUB_REF_NAME')
     print(f'github_ref_name is: {github_ref_name}')
-    github_run_id = os.environ['GITHUB_RUN_ID']
+    github_run_id = os.environ.get('GITHUB_RUN_ID')
     print(f'github_run_id is: {github_run_id}')
-    github_sha = os.environ['GITHUB_SHA']
+    github_sha = os.environ.get('GITHUB_SHA')
     print(f'github_sha is: {github_sha}')
-    github_runner = os.environ['RUNNER_NAME']
+    github_runner = os.environ.get('RUNNER_NAME')
     print(f'github_runner is: {github_runner}')
     created_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     product_name = 'juicefs'
     db = MySQLdb.connect(host="8.210.231.144", user="juicedata", passwd=passowrd, db="test_result")
     c = db.cursor()
     c.execute("insert into benchmark(name, result, product_name, product_version, meta, storage, github_ref_name, github_run_id, github_sha, github_runner, created_date) \
-        values(%s, %s)", (name, result, product_name, product_version, meta, storage, github_ref_name, github_run_id, github_sha, github_runner, created_date))
+        values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (name, result, product_name, product_version, meta, storage, github_ref_name, github_run_id, github_sha, github_runner, created_date))
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
