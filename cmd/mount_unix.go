fd407051ccf664f8ed15099df1746d7e903c8ea6
xixi <i@hexilee.me>
2022-11-14 17:06:34 +0800

    remove short flag of --force
    Signed-off-by: xixi <i@hexilee.me>

--- cmd/mount_unix.go
+++ cmd/mount_unix.go
@@ -141,9 +141,8 @@ func mount_flags() []cli.Flag {
 			Usage: "enable ioctl (support GETFLAGS/SETFLAGS only)",
 		},
 		&cli.BoolFlag{
-			Name:    "f",
-			Aliases: []string{"force"},
-			Usage:   "force to mount even if the mount point is already mounted by the same filesystem",
+			Name:  "force",
+			Usage: "force to mount even if the mount point is already mounted by the same filesystem",
 		},
 		&cli.BoolFlag{
 			Name:  "update-fstab",
