ac489be3514d0bc9ff3e1c72a37dae15694db2e9
Zhou Cheng <zhoucheng@juicedata.io>
2022-11-30 22:57:44 +0800

    debug

--- .github/scripts/mutesting.py
+++ .github/scripts/mutesting.py
@@ -2,26 +2,28 @@
 # -*- encoding: utf-8 -*-
 import glob
 import os
+import sys
+
 
 def do_mutate_test(mutation_dir, index, total):
-    print(f'mutation dir is {mutation_dir}, inde is {index}, total is {total}')
-    os.system(f'ls -l {mutation_dir}')
+    print(f'mutation dir is {mutation_dir}, inde is {index}, total is {total}', file=sys.stderr)
+    # os.system(f'ls -l {mutation_dir}')
     list_of_files = filter( os.path.isfile, glob.glob(mutation_dir + '/*') )
     list_of_files = sorted( list_of_files, key = os.path.getmtime)
-    stats = {'passed':0, 'failed':0, 'compile_error':0, 'out_of_coverage':0, 'skip_by_comment':0}
+    print('\n'.join(list_of_files), file=sys.stderr)
+    stats = {'passed':0, 'failed':0, 'compile_error':0, 'out_of_coverage':0, 'skip_by_comment':0, 'others':0, 'total':0}
     count = int(len(list_of_files)/total) + 1
     start = index*count
     end = start + count
-    print(f'count:{count}, start:{start}, end:{end}')
+    print(f'count:{count}, start:{start}, end:{end}', file=sys.stderr)
     if end > len(list_of_files):
         end = len(list_of_files)
     for changed_file in list_of_files[start:end]:
         # timestamp_str = time.strftime(  '%m/%d/%Y :: %H:%M:%S',
         #                             time.gmtime(os.path.getmtime(changed_file))) 
         # print(timestamp_str, ' -->', changed_file) 
-        os.environ['MUTATE_PACKAGE'] = ''
         os.environ['MUTATE_CHANGED'] = changed_file
-        print('-----------------------------------------------------------------')
+        print('-----------------------------------------------------------------', file=sys.stderr)
         ret = os.system('.github/scripts/mutest.sh') >> 8
         if ret == 0:
             stats['passed'] += 1
@@ -33,13 +35,21 @@ def do_mutate_test(mutation_dir, index, total):
             stats['out_of_coverage'] += 1
         elif ret == 102:
             stats['skip_by_comment'] += 1
+        else:
+            stats['others'] += 1
+        stats['total'] += 1
+    if stats['total'] == 0:
+        stats['score'] = 1.0
+    else
+        stats['score'] = stats['passed'] / stats['total']
     return stats
 
 if __name__ == '__main__':
+    os.environ['MUTATE_PACKAGE'] = ''
     mutation_dir = os.path.join(os.environ['MUTATION_DIR'], os.environ['PACKAGE_PATH'])
-    print(f'mutation dir is {mutation_dir}')
+    print(f'mutation dir is {mutation_dir}', file=sys.stderr)
     original_file = os.environ['MUTATE_ORIGINAL']
-    print(f'original file is {original_file}')
+    print(f'original file is {original_file}', file=sys.stderr)
     if not os.environ['JOB_INDEX']:
         index = 0
     else:
