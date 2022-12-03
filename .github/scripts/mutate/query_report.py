715822f3b4def99edd2f4c17cd6090468332d994
Zhou Cheng <zhoucheng@juicedata.io>
2022-12-03 18:59:00 +0800

    debug

--- .github/scripts/mutate/query_report.py
+++ .github/scripts/mutate/query_report.py
@@ -2,9 +2,7 @@
 import os
 import MySQLdb
 
-def query_report():
-    repo = os.environ.get('GITHUB_REPOSITORY')
-    run_id = os.environ.get('GITHUB_RUN_ID')    
+def query_report(repo, run_id):
     passowrd = os.environ['MYSQL_PASSWORD']
     db = MySQLdb.connect(host="8.210.231.144", user="juicedata", passwd=passowrd, db="mutate")
     db.query(f"""SELECT job_name, github_job_url, passed, failed, compile_error, out_of_coverage, skip_by_comment, others FROM report 
@@ -16,4 +14,10 @@ def query_report():
     db.close()
 
 if __name__ == "__main__":
-    query_report()
+    repo = os.environ.get('GITHUB_REPOSITORY')
+    run_id = os.environ.get('GITHUB_RUN_ID')
+    if os.environ.get('REPO'): 
+        repo = os.environ.get('REPO')
+    if os.environ.get('RUN_ID'): 
+        run_id = os.environ.get('RUN_ID')
+    query_report(repo, run_id)
