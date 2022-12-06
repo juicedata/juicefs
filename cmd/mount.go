60047378947e3e1647f737cde2d71cae26028bf9
xixi <hexilee@juicedata.io>
2022-12-06 17:27:47 +0800

    inlined some functions
    Signed-off-by: xixi <hexilee@juicedata.io>

--- cmd/mount.go
+++ cmd/mount.go
@@ -271,33 +271,33 @@ func configEqual(a, b *vfs.Config) bool {
 	if a == nil || b == nil {
 		return a == b
 	}
+
 	ac, bc := *a, *b
 	ac.Meta, ac.Chunk, ac.Format, ac.Port, ac.AttrTimeout, ac.DirEntryTimeout, ac.EntryTimeout = nil, nil, nil, nil, 0, 0, 0
 	bc.Meta, bc.Chunk, bc.Format, bc.Port, bc.AttrTimeout, bc.DirEntryTimeout, bc.EntryTimeout = nil, nil, nil, nil, 0, 0, 0
-	return ac == bc && metaEq(a.Meta, b.Meta) && chunkEq(a.Chunk, b.Chunk) && formatEq(a.Format, b.Format)
-}
+	eq := ac == bc
 
-func formatEq(a, b *meta.Format) bool {
-	if a == nil || b == nil {
-		return a == b
+	if a.Format == nil || b.Format == nil {
+		eq = eq && a.Format == b.Format
+	} else {
+		af, bf := *a.Format, *b.Format
+		af.SecretKey, bf.SecretKey = "", ""
+		eq = eq && af == bf
 	}
-	af, bf := *a, *b
-	af.SecretKey, bf.SecretKey = "", ""
-	return af == bf
-}
 
-func metaEq(a, b *meta.Config) bool {
-	if a == nil || b == nil {
-		return a == b
+	if a.Meta == nil || b.Meta == nil {
+		eq = eq && a.Meta == b.Meta
+	} else {
+		eq = eq && *a.Meta == *b.Meta
 	}
-	return *a == *b
-}
 
-func chunkEq(a, b *chunk.Config) bool {
-	if a == nil || b == nil {
-		return a == b
+	if a.Chunk == nil || b.Chunk == nil {
+		eq = eq && a.Chunk == b.Chunk
+	} else {
+		eq = eq && *a.Chunk == *b.Chunk
 	}
-	return *a == *b
+
+	return eq
 }
 
 func prepareMp(newCfg *vfs.Config, mp string) (ignore bool) {
