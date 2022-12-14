2088626afaa2ff46b0c85c7b55dba3e648bb3bb3
xixi <hexilee@juicedata.io>
2022-12-14 15:48:41 +0800

    status: rename DeletedSlice to TrashSlice
    Signed-off-by: xixi <hexilee@juicedata.io>

--- cmd/status.go
+++ cmd/status.go
@@ -64,14 +64,14 @@ type sections struct {
 }
 
 type statistic struct {
-	UsedSpace         uint64
-	AvailableSpace    uint64
-	UsedInodes        uint64
-	AvailableInodes   uint64
-	DeletedSliceCount int64 `json:",omitempty"`
-	DeletedSliceSize  int64 `json:",omitempty"`
-	DeletedFileCount  int64 `json:",omitempty"`
-	DeletedFileSize   int64 `json:",omitempty"`
+	UsedSpace        uint64
+	AvailableSpace   uint64
+	UsedInodes       uint64
+	AvailableInodes  uint64
+	TrashSliceCount  int64 `json:",omitempty"`
+	TrashSliceSize   int64 `json:",omitempty"`
+	DeletedFileCount int64 `json:",omitempty"`
+	DeletedFileSize  int64 `json:",omitempty"`
 }
 
 func printJson(v interface{}) {
@@ -115,9 +115,9 @@ func status(ctx *cli.Context) error {
 
 	if ctx.Bool("more") {
 		progress := utils.NewProgress(false, false)
-		slicesSpinner := progress.AddDoubleSpinner("Delayed Slices")
+		slicesSpinner := progress.AddDoubleSpinner("Trash Slices")
 		defer slicesSpinner.Done()
-		fileSpinner := progress.AddDoubleSpinner("Delayed Files")
+		fileSpinner := progress.AddDoubleSpinner("Deleted Files")
 		defer fileSpinner.Done()
 
 		err = m.ScanDeletedObject(
@@ -137,7 +137,7 @@ func status(ctx *cli.Context) error {
 			logger.Fatalf("statistic: %s", err)
 			return err
 		}
-		stat.DeletedSliceCount, stat.DeletedSliceSize = slicesSpinner.Current()
+		stat.TrashSliceCount, stat.TrashSliceSize = slicesSpinner.Current()
 		stat.DeletedFileCount, stat.DeletedFileSize = fileSpinner.Current()
 	}
 
