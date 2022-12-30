18d4b0b4fe8060686ea24d9470a60ba9a39994b1
zhijian <djx@juicedata.io>
2022-12-30 11:16:22 +0800

    object/OVH: support OVH region (#3133)

--- pkg/object/object_storage_test.go
+++ pkg/object/object_storage_test.go
@@ -28,6 +28,7 @@ import (
 	"math"
 	"os"
 	"reflect"
+	"regexp"
 	"sort"
 	"strings"
 	"testing"
@@ -353,6 +354,39 @@ func TestS3(t *testing.T) {
 		os.Getenv("AWS_SECRET_ACCESS_KEY"),
 		os.Getenv("AWS_SESSION_TOKEN"))
 	testStorage(t, s)
+}
+
+func TestOracleCompileRegexp(t *testing.T) {
+	ep := "axntujn0ebj1.compat.objectstorage.ap-singapore-1.oraclecloud.com"
+	oracleCompile := regexp.MustCompile(oracleCompileRegexp)
+	if oracleCompile.MatchString(ep) {
+		if submatch := oracleCompile.FindStringSubmatch(ep); len(submatch) >= 2 {
+			if submatch[1] != "ap-singapore-1" {
+				t.Fatalf("oracle endpoint parse failed")
+			}
+		} else {
+			t.Fatalf("oracle endpoint parse failed")
+		}
+	} else {
+		t.Fatalf("oracle endpoint parse failed")
+	}
+}
+
+func TestOVHCompileRegexp(t *testing.T) {
+	for _, ep := range []string{"s3.gra.cloud.ovh.net", "s3.gra.perf.cloud.ovh.net", "s3.gra.io.cloud.ovh.net"} {
+		ovhCompile := regexp.MustCompile(OVHCompileRegexp)
+		if ovhCompile.MatchString(ep) {
+			if submatch := ovhCompile.FindStringSubmatch(ep); len(submatch) >= 2 {
+				if submatch[1] != "gra" {
+					t.Fatalf("ovh endpoint parse failed")
+				}
+			} else {
+				t.Fatalf("ovh endpoint parse failed")
+			}
+		} else {
+			t.Fatalf("ovh endpoint parse failed")
+		}
+	}
 }
 
 func TestOSS(t *testing.T) {
