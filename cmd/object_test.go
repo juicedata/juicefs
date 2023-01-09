7601511d610513906596ab44d1afc101eee12543
Davies Liu <davies@juicedata.io>
2023-01-09 16:18:51 +0800

    add license header

--- cmd/object_test.go
+++ cmd/object_test.go
@@ -1,3 +1,18 @@
+/*
+ * JuiceFS, Copyright 2020 Juicedata, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package cmd
 
 import (
@@ -33,6 +48,7 @@ func testKeysEqual(objs []object.Object, expectedKeys []string) error {
 	return nil
 }
 
+// copied from pkg/object/filesystem_test.go
 func testFileSystem(t *testing.T, s object.ObjectStorage) {
 	keys := []string{
 		"x/",
