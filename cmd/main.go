4a8b091196530a76634dc71c28e974251287d084
zhijian <djx@juicedata.io>
2023-02-22 12:41:05 +0800

    dont set the default schema when remove password form titles (#3258)

--- cmd/main.go
+++ cmd/main.go
@@ -295,12 +295,7 @@ func setup(c *cli.Context, n int) {
 }
 
 func removePassword(uri string) {
-	var uri2 string
-	if strings.Contains(uri, "://") {
-		uri2 = utils.RemovePassword(uri)
-	} else {
-		uri2 = utils.RemovePassword("redis://" + uri)
-	}
+	uri2 := utils.RemovePassword(uri)
 	if uri2 != uri {
 		for i, a := range os.Args {
 			if a == uri {
