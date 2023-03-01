c33e76072b50fddcbd74863dd23fae8296faa12d
zhijian <djx@juicedata.io>
2023-03-01 14:29:21 +0800

    set delimiter when it is not empty string (#3273)

--- pkg/object/ks3.go
+++ pkg/object/ks3.go
@@ -148,8 +148,10 @@ func (s *ks3) List(prefix, marker, delimiter string, limit int64) ([]Object, err
 		Prefix:       &prefix,
 		Marker:       &marker,
 		MaxKeys:      &limit,
-		Delimiter:    &delimiter,
 		EncodingType: aws.String("url"),
+	}
+	if delimiter != "" {
+		param.Delimiter = &delimiter
 	}
 	resp, err := s.s3.ListObjects(&param)
 	if err != nil {
