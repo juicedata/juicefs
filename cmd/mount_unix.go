4fa7da5634161e644b76c6b83b6c35c67b9c7eda
xixi <hexilee@juicedata.io>
2023-03-15 13:46:34 +0800

    print more messages
    Signed-off-by: xixi <hexilee@juicedata.io>

--- cmd/mount_unix.go
+++ cmd/mount_unix.go
@@ -19,23 +19,12 @@
 
 package cmd
 
-// #include <sys/sysmacros.h>
-// #include <sys/types.h>
-// // makedev is a macro, so a wrapper is needed
-// dev_t Makedev(unsigned int maj, unsigned int min) {
-//   return makedev(maj, min);
-// }
-import "C"
-
 import (
-	"bufio"
 	"bytes"
-	"fmt"
 	"os"
 	"path"
 	"path/filepath"
 	"runtime"
-	"strings"
 	"syscall"
 	"time"
 
@@ -43,7 +32,6 @@ import (
 	"github.com/juicedata/juicefs/pkg/fuse"
 	"github.com/juicedata/juicefs/pkg/meta"
 	"github.com/juicedata/juicefs/pkg/vfs"
-	"github.com/pkg/errors"
 	"github.com/urfave/cli/v2"
 )
 
@@ -201,8 +189,8 @@ func mount_main(v *vfs.VFS, c *cli.Context) {
 		disableUpdatedb()
 	}
 	if c.Bool("grant-access") {
-		ensureFuseDev()
-		err := grantAccess()
+		fuse.EnsureFuseDev()
+		err := fuse.GrantAccess()
 		if err != nil {
 			logger.Error("fail to grant access to /dev/fuse: ", err)
 		}
@@ -216,61 +204,4 @@ func mount_main(v *vfs.VFS, c *cli.Context) {
 	if err != nil {
 		logger.Fatalf("fuse: %s", err)
 	}
-}
-
-// ensureFuseDev ensures /dev/fuse exists. If not, it will create one
-func ensureFuseDev() {
-	if _, err := os.Open("/dev/fuse"); os.IsNotExist(err) {
-		// 10, 229 according to https://www.kernel.org/doc/Documentation/admin-guide/devices.txt
-		fuse := C.Makedev(10, 229)
-		syscall.Mknod("/dev/fuse", 0o666|syscall.S_IFCHR, int(fuse))
-	}
-}
-
-// grantAccess appends 'c 10:229 rwm' to devices.allow
-func grantAccess() error {
-	pid := os.Getpid()
-	cgroupPath := fmt.Sprintf("/proc/%d/cgroup", pid)
-	cgroupFile, err := os.Open(cgroupPath)
-	if err != nil {
-		return err
-	}
-
-	// TODO: encapsulate these logic with chaos-daemon StressChaos part
-	cgroupScanner := bufio.NewScanner(cgroupFile)
-	var deviceCgroupPath string
-	for cgroupScanner.Scan() {
-		if err := cgroupScanner.Err(); err != nil {
-			return err
-		}
-		var (
-			text  = cgroupScanner.Text()
-			parts = strings.SplitN(text, ":", 3)
-		)
-		if len(parts) < 3 {
-			return errors.Errorf("invalid cgroup entry: %q", text)
-		}
-
-		if parts[1] == "devices" {
-			deviceCgroupPath = parts[2]
-		}
-	}
-
-	if len(deviceCgroupPath) == 0 {
-		return errors.Errorf("fail to find device cgroup")
-	}
-
-	deviceCgroupPath = "/sys/fs/cgroup/devices" + deviceCgroupPath + "/devices.allow"
-	f, err := os.OpenFile(deviceCgroupPath, os.O_WRONLY, 0)
-	if err != nil {
-		return err
-	}
-	// 10, 229 according to https://www.kernel.org/doc/Documentation/admin-guide/devices.txt
-	content := "c 10:229 rwm"
-	_, err = f.WriteString(content)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
