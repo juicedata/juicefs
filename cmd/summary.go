7bbec0ec550eee86941a348c08fd1da919777fe0
xixi <hexilee@juicedata.io>
2023-03-27 11:18:09 +0800

    update license header
    Signed-off-by: xixi <hexilee@juicedata.io>

--- cmd/summary.go
+++ cmd/summary.go
@@ -1,5 +1,5 @@
 /*
- * JuiceFS, Copyright 2020 Juicedata, Inc.
+ * JuiceFS, Copyright 2023 Juicedata, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
