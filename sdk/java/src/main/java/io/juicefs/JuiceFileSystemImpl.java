ebcfd2eafb212d2810a5d287b79ba37ea0d729ae
tangyoupeng <tommy@juicedata.io>
2023-03-30 12:18:34 +0800

    hadoop: up sdk ver

--- sdk/java/src/main/java/io/juicefs/JuiceFileSystemImpl.java
+++ sdk/java/src/main/java/io/juicefs/JuiceFileSystemImpl.java
@@ -509,7 +509,7 @@ public class JuiceFileSystemImpl extends FileSystem {
     LibraryLoader<Libjfs> libjfsLibraryLoader = LibraryLoader.create(Libjfs.class);
     libjfsLibraryLoader.failImmediately();
 
-    int soVer = 7;
+    int soVer = 8;
     String osId = "so";
     String archId = "amd64";
     String resourceFormat = "libjfs-%s.%s.gz";
