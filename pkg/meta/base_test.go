c0192c837ecfaef22825c33f3dcb66a14c63e393
zhijian <djx@juicedata.io>
2023-04-17 10:28:08 +0800

    adjust

--- pkg/meta/base_test.go
+++ pkg/meta/base_test.go
@@ -2199,7 +2199,7 @@ func testClone(t *testing.T, m Meta) {
 	}
 	m.StatFS(Background, cloneDir, &totalspace, &availspace, &iused, &iavail)
 	if totalspace-availspace-space != 32768 {
-		time.Sleep(time.Second * 5)
+		time.Sleep(time.Second * 2)
 		m.StatFS(Background, cloneDir, &totalspace, &availspace, &iused, &iavail)
 		if totalspace-availspace-space != 32768 {
 			t.Fatalf("added space: %d", totalspace-availspace-space)
