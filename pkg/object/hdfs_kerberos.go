477e2318a37a27ef8bdafce1db637c6b225464af
kyungwan-nam <kw.nam@navercorp.com>
2023-04-25 12:35:22 +0900

    Support keytab (#3517)
    * Support kerberos keytab for hdfs authentication
    
    * Support kerberos keytab for hdfs authentication

--- pkg/object/hdfs_kerberos.go
+++ pkg/object/hdfs_kerberos.go
@@ -8,6 +8,7 @@ package object
 
 import (
 	"fmt"
+	"github.com/jcmturner/gokrb5/v8/keytab"
 	"os"
 	"os/user"
 	"strings"
@@ -26,6 +27,25 @@ func getKerberosClient() (*krb.Client, error) {
 	cfg, err := config.Load(configPath)
 	if err != nil {
 		return nil, err
+	}
+
+	// Try to authenticate with keytab file first.
+	keytabPath := os.Getenv("KRB5KEYTAB")
+	principal := os.Getenv("KRB5PRINCIPAL")
+	if keytabPath != "" && principal != "" {
+		kt, err := keytab.Load(keytabPath)
+		if err != nil {
+			return nil, err
+		}
+		// e.g. KRB5PRINCIPAL="primary/instance@realm"
+		sp := strings.Split(principal, "@")
+		if len(sp) != 2 {
+			return nil, fmt.Errorf("unusable kerberos principal: %s", principal)
+		}
+		username, realm := sp[0], sp[1]
+		logger.Infof("username: %s, realm: %s", username, realm)
+		client := krb.NewWithKeytab(username, realm, kt, cfg)
+		return client, nil
 	}
 
 	// Determine the ccache location from the environment, falling back to the
