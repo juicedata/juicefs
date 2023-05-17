2cacb26697c41dd4096501a298d7b32ac0593d9f
xixi <hexilee@juicedata.io>
2023-05-18 01:28:24 +0800

    wrapContext enable CheckPermission by default
    Signed-off-by: xixi <hexilee@juicedata.io>

--- pkg/meta/context.go
+++ pkg/meta/context.go
@@ -80,7 +80,7 @@ func (c *wrapContext) WithValue(k, v interface{}) {
 }
 
 func (c *wrapContext) CheckPermission() bool {
-	return false
+	return true
 }
 
 func NewContext(pid, uid uint32, gids []uint32) Context {
