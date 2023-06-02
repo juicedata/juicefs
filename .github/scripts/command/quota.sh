b91060942963ef872c51923d8d2e60b4451f23a3
Zhou Cheng <zhoucheng@juicedata.io>
2023-06-02 15:02:33 +0800

    fix bug

--- .github/scripts/command/quota.sh
+++ .github/scripts/command/quota.sh
@@ -7,34 +7,36 @@ source .github/scripts/start_meta_engine.sh
 start_meta_engine $META
 META_URL=$(get_meta_url $META)
 
-
+HEARTBEAT_INTERVAL=5
+DIR_QUOTA_FLUSH_INTERVAL=4
+VOLUME_QUOTA_FLUSH_INTERVAL=2
 source .github/scripts/common/common.sh
 
 test_total_capacity()
 {
     prepare_test
     ./juicefs format $META_URL myjfs --capacity 1
-    ./juicefs mount -d $META_URL /jfs --heartbeat 5 --debug
+    ./juicefs mount -d $META_URL /jfs --heartbeat $HEARTBEAT_INTERVAL --debug
     dd if=/dev/zero of=/jfs/test1 bs=1G count=1
-    sleep 10s
+    sleep $VOLUME_QUOTA_FLUSH_INTERVAL
     echo a | tee -a /jfs/test1 2>error.log && echo "echo should fail on out of space" && exit 1 || true
     grep "No space left on device" error.log
     ./juicefs config $META_URL --capacity 2
-    sleep 10s
+    sleep $((HEARTBEAT_INTERVAL+1))
     dd if=/dev/zero of=/jfs/test2 bs=1G count=1
-    sleep 10s
+    sleep $VOLUME_QUOTA_FLUSH_INTERVAL
     echo a | tee -a /jfs/test2 2>error.log && echo "echo should fail on out of space" && exit 1 || true
     grep "No space left on device" error.log
 
     rm /jfs/test1 -rf
-    sleep 10s
+    sleep $VOLUME_QUOTA_FLUSH_INTERVAL
     echo a | tee -a /jfs/test3 2>error.log && echo "echo should fail on out of space" && exit 1 || true
 
     ./juicefs rmr /jfs/.trash
-    sleep 10s
+    sleep $VOLUME_QUOTA_FLUSH_INTERVAL
     echo a | tee -a /jfs/test3 
 
-    sleep 10s
+    sleep $VOLUME_QUOTA_FLUSH_INTERVAL
     ln /jfs/test2 /jfs/test4
     ln /jfs/test2 /jfs/test5
 }
@@ -42,35 +44,35 @@ test_total_capacity()
 test_total_inodes(){
     prepare_test
     ./juicefs format $META_URL myjfs --inodes 1000
-    ./juicefs mount -d $META_URL /jfs --heartbeat 5
+    ./juicefs mount -d $META_URL /jfs --heartbeat $HEARTBEAT_INTERVAL
     set +x
     for i in {1..1000}; do
-        echo $i | tee /jfs/test$i
+        echo $i | tee /jfs/test$i > /dev/null
     done
     set -x
-    sleep 12s
+    sleep $VOLUME_QUOTA_FLUSH_INTERVAL
     echo a | tee /jfs/test1001 2>error.log && echo "write should fail on out of inodes" && exit 1 || true
     grep "No space left on device" error.log
     ./juicefs config $META_URL --inodes 2000
     sleep 6s
     set +x
     for i in {1001..2000}; do
-        echo $i | tee /jfs/test$i
+        echo $i | tee /jfs/test$i > /dev/null
     done
     set -x
-    sleep 6s
+    sleep $VOLUME_QUOTA_FLUSH_INTERVAL
     echo a | tee /jfs/test2001 2>error.log && echo "write should fail on out of inodes" && exit 1 || true
 }
 
 test_dir_capacity(){
     prepare_test
     ./juicefs format $META_URL myjfs
-    ./juicefs mount -d $META_URL /jfs --heartbeat 5
+    ./juicefs mount -d $META_URL /jfs --heartbeat $HEARTBEAT_INTERVAL
     mkdir -p /jfs/d
     ./juicefs quota set $META_URL --path /d --capacity 1
-    sleep 6s
+    sleep $((HEARTBEAT_INTERVAL+1))
     dd if=/dev/zero of=/jfs/d/test1 bs=1G count=1
-    sleep 3s
+    sleep $DIR_QUOTA_FLUSH_INTERVAL
     ./juicefs quota get $META_URL --path /d
     used=$(./juicefs quota get $META_URL --path /d 2>&1 | grep "/d" | awk -F'|' '{print $5}'  | tr -d '[:space:]')
     [[ $used != "100%" ]] && echo "used should be 100%" && exit 1 || true
@@ -78,60 +80,60 @@ test_dir_capacity(){
     grep -i "Disk quota exceeded" error.log || (echo "grep failed" && exit 1)
 
     ./juicefs quota set $META_URL --path /d --capacity 2
-    sleep 6s
+    sleep $((HEARTBEAT_INTERVAL+1))
     dd if=/dev/zero of=/jfs/d/test2 bs=1G count=1
-    sleep 3s
+    sleep $DIR_QUOTA_FLUSH_INTERVAL
     echo a | tee -a /jfs/d/test2 2>error.log && echo "echo should fail on out of space" && exit 1 || true
     grep -i "Disk quota exceeded" error.log || (echo "grep failed" && exit 1)
     rm -rf /jfs/d/test1
-    sleep 3s
+    sleep $DIR_QUOTA_FLUSH_INTERVAL
     used=$(./juicefs quota get $META_URL --path /d 2>&1 | grep "/d" | awk -F'|' '{print $5}'  | tr -d '[:space:]')
     [[ $used != "50%" ]] && echo "used should be 50%" && exit 1 || true
     dd if=/dev/zero of=/jfs/d/test3 bs=1G count=1
-    sleep 3s
+    sleep $DIR_QUOTA_FLUSH_INTERVAL
     ./juicefs quota check $META_URL --path /d --strict
 }
 
 test_dir_inodes(){
     prepare_test
     ./juicefs format $META_URL myjfs 
-    ./juicefs mount -d $META_URL /jfs --heartbeat 5
+    ./juicefs mount -d $META_URL /jfs --heartbeat $HEARTBEAT_INTERVAL
     mkdir -p /jfs/d
     ./juicefs quota set $META_URL --path /d --inodes 1000
-    sleep 6s
+    sleep $((HEARTBEAT_INTERVAL+1))
     set +x
     for i in {1..1000}; do
-        echo $i > /jfs/d/test$i
+        echo $i > /jfs/d/test$i > /dev/null
     done
     set -x
-    sleep 3s
+    sleep $DIR_QUOTA_FLUSH_INTERVAL
     echo a | tee /jfs/d/test1001 2>error.log && echo "write should fail on out of inodes" && exit 1 || true
     grep "Disk quota exceeded" error.log || (echo "grep failed" && exit 1)
     rm -rf error.log
     ./juicefs quota set $META_URL --path /d --inodes 2000
-    sleep 6s
+    sleep $((HEARTBEAT_INTERVAL+1))
     set +x
     for i in {1001..2000}; do
-        echo $i | tee  /jfs/d/test$i
+        echo $i | tee  /jfs/d/test$i > /dev/null
     done
     set -x
-    sleep 3s
+    sleep $DIR_QUOTA_FLUSH_INTERVAL
     echo a | tee  /jfs/d/test2001 2>error.log && echo "write should fail on out of inodes" && exit 1 || true
     grep "Disk quota exceeded" error.log || (echo "grep failed" && exit 1)
     rm /jfs/d/test1 -rf
-    sleep 3s
+    sleep $DIR_QUOTA_FLUSH_INTERVAL
     echo a | tee  /jfs/d/test2001
-    sleep 3s
+    sleep $DIR_QUOTA_FLUSH_INTERVAL
     ./juicefs quota check $META_URL --path /d --strict
 }
 
 test_sub_dir(){
     prepare_test
     ./juicefs format $META_URL myjfs 
-    ./juicefs mount -d $META_URL /jfs --heartbeat 5
+    ./juicefs mount -d $META_URL /jfs --heartbeat $HEARTBEAT_INTERVAL
     mkdir -p /jfs/d
     ./juicefs quota set $META_URL --path /d --inodes 1000 --capacity 1
-    sleep 6s
+    sleep $((HEARTBEAT_INTERVAL+1))
     umount_jfs /jfs $META_URL
     ./juicefs mount -d $META_URL --subdir /d /jfs --heartbeat 2
     size=$(df -h /jfs | grep "JuiceFS" | awk '{print $2}')
@@ -139,17 +141,17 @@ test_sub_dir(){
     inodes=$(df -ih /jfs | grep "JuiceFS" | awk '{print $2}')
     [[ $inodes != "1000" ]] && echo "inodes should be 1000" && exit 1 || true
     dd if=/dev/zero of=/jfs/test1 bs=1G count=1
-    sleep 3s
+    sleep $DIR_QUOTA_FLUSH_INTERVAL
     echo a | tee -a /jfs/test1 2>error.log && echo "write should fail on out of space" && exit 1 || true
     grep "Disk quota exceeded" error.log || (echo "grep failed" && exit 1)
     rm /jfs/test1 -rf
-    sleep 3s
+    sleep $DIR_QUOTA_FLUSH_INTERVAL
     set +x
     for i in {1..1000}; do
-        echo $i | tee /jfs/test$i
+        echo $i | tee /jfs/test$i > /dev/null
     done
     set -x
-    sleep 3s
+    sleep $DIR_QUOTA_FLUSH_INTERVAL
     echo $i | tee /jfs/test1001 2>error.log && echo "write should fail on out of inodes" && exit 1 || true
     grep "Disk quota exceeded" error.log || (echo "grep failed" && exit 1)
     ./juicefs quota check $META_URL --path /d --strict
@@ -158,24 +160,24 @@ test_sub_dir(){
 test_dump_load(){
     prepare_test
     ./juicefs format $META_URL myjfs 
-    ./juicefs mount -d $META_URL /jfs --heartbeat 5
+    ./juicefs mount -d $META_URL /jfs --heartbeat $HEARTBEAT_INTERVAL
     mkdir -p /jfs/d
     ./juicefs quota set $META_URL --path /d --inodes 1000 --capacity 1
-    sleep 6s
+    sleep $((HEARTBEAT_INTERVAL+1))
     ./juicefs dump $META_URL > dump.json
     umount_jfs /jfs $META_URL
     python3 .github/scripts/flush_meta.py $META_URL
     ./juicefs load $META_URL dump.json
     ./juicefs mount $META_URL /jfs -d --heartbeat 5
     dd if=/dev/zero of=/jfs/d/test1 bs=1G count=1
-    sleep 3s
+    sleep $DIR_QUOTA_FLUSH_INTERVAL
     echo a | tee -a /jfs/d/test1 2>error.log && echo "write should fail on out of space" && exit 1 || true
     grep "Disk quota exceeded" error.log || (echo "grep failed" && exit 1)
     rm /jfs/d/test1 -rf
-    sleep 3s
+    sleep $DIR_QUOTA_FLUSH_INTERVAL
     set +x
     for i in {1..1000}; do
-        echo $i | tee /jfs/d/test$i
+        echo $i | tee /jfs/d/test$i > /dev/null
     done
     set -x
     sleep 3s
@@ -187,32 +189,32 @@ test_dump_load(){
 test_hard_link(){
     prepare_test
     ./juicefs format $META_URL myjfs 
-    ./juicefs mount -d $META_URL /jfs --heartbeat 5
+    ./juicefs mount -d $META_URL /jfs --heartbeat $HEARTBEAT_INTERVAL
     mkdir -p /jfs/d
     dd if=/dev/zero of=/jfs/file bs=1G count=1
     ./juicefs quota set $META_URL --path /d --capacity 2
-    sleep 6s
+    sleep $((HEARTBEAT_INTERVAL+1))
     dd if=/dev/zero of=/jfs/d/test1 bs=1G count=1
-    sleep 3s
+    sleep $DIR_QUOTA_FLUSH_INTERVAL
     ln /jfs/file /jfs/d/test2
-    sleep 3s
+    sleep $DIR_QUOTA_FLUSH_INTERVAL
     ln /jfs/file /jfs/d/test3 2>error.log && echo "hard link should fail on out of space" && exit 1 || true
     grep "Disk quota exceeded" error.log || (echo "grep failed" && exit 1)
-    sleep 3s
+    sleep $DIR_QUOTA_FLUSH_INTERVAL
     ./juicefs quota check $META_URL --path /d --strict
 }
 
 test_check_and_repair_quota(){
     prepare_test
     ./juicefs format $META_URL myjfs 
-    ./juicefs mount -d $META_URL /jfs --heartbeat 5
+    ./juicefs mount -d $META_URL /jfs --heartbeat $HEARTBEAT_INTERVAL
     mkdir -p /jfs/d
     ./juicefs quota set $META_URL --path /d --capacity 1
-    sleep 6s
+    sleep $((HEARTBEAT_INTERVAL+1))
     dd if=/dev/zero of=/jfs/d/test1 bs=1G count=1
     pid=$(ps -ef | grep "juicefs mount" | grep -v grep | awk '{print $2}')
     kill -9 $pid
-    sleep 3s
+    sleep $DIR_QUOTA_FLUSH_INTERVAL
     # ./juicefs quota check $META_URL --path /d --strict && echo "quota check should fail" && exit 1 || true
     ./juicefs quota check $META_URL --path /d --strict --repair
     ./juicefs quota check $META_URL --path /d --strict
