e3530c05e8c50d94505613ffcb9b15db1348c6b7
Sandy Xu <sandy@juicedata.io>
2023-06-08 16:03:06 +0800

    hadoop: change back the so version

--- sdk/java/src/main/java/io/juicefs/JuiceFileSystemImpl.java
+++ sdk/java/src/main/java/io/juicefs/JuiceFileSystemImpl.java
@@ -581,7 +581,7 @@ public class JuiceFileSystemImpl extends FileSystem {
     LibraryLoader<Libjfs> libjfsLibraryLoader = LibraryLoader.create(Libjfs.class);
     libjfsLibraryLoader.failImmediately();
 
-    int soVer = 8;
+    int soVer = 7;
     String osId = "so";
     String archId = "amd64";
     String resourceFormat = "libjfs-%s.%s.gz";
