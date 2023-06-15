465f9f9648c0a05930085724391256ca7da6ee86
Sandy Xu <sandy@juicedata.io>
2023-06-15 13:51:09 +0800

    meta: don't compact chunk again after the previous one completed (#3810)

--- pkg/meta/tkv.go
+++ pkg/meta/tkv.go
@@ -2471,12 +2471,6 @@ func (m *kvMeta) compactChunk(inode Ino, indx uint32, force bool) {
 
 	if force {
 		m.compactChunk(inode, indx, force)
-	} else {
-		go func() {
-			// wait for the current compaction to finish
-			time.Sleep(time.Minute)
-			m.compactChunk(inode, indx, force)
-		}()
 	}
 }
 
