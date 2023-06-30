47de8adb45c0cdae76f7251e7e880e3677698d67
Cheng Zhou <zhoucheng@juicedata.io>
2023-06-30 14:55:06 +0800

    wip

--- .github/scripts/command/load_dump_bench.sh
+++ .github/scripts/command/load_dump_bench.sh
@@ -20,7 +20,6 @@ test_load_dump_with_small_dir(){
   ./juicefs load $META_URL $load_file
   end=`date +%s`
   runtime=$((end-start))
-  export MYSQL_PASSWORD=${{secrets.MYSQL_PASSWORD_FOR_JUICEDATA}} 
   sudo chmod +x .github/scripts/db.py
   version=$(./juicefs -V|cut -b 17- | sed 's/:/-/g')
   python3 .github/scripts/db.py --name load_small_dir --result $runtime --version $version --meta ${{matrix.meta}} --storage file
@@ -41,7 +40,16 @@ test_load_dump_with_small_dir(){
   fi
 }
 
+test_load_dump_with_big_dir_subdir(){
+  do_load_dump_with_big_dir true
+}
+
 test_load_dump_with_big_dir(){
+  do_load_dump_with_big_dir false
+}
+
+do_load_dump_with_big_dir(){
+  with_subdir=$1
   prepare_test
   create_database $META_URL
   echo meta_url is: $META_URL
@@ -54,12 +62,15 @@ test_load_dump_with_big_dir(){
   end=`date +%s`
   runtime=$((end-start))
   echo "load cost $runtime seconds"
-  export MYSQL_PASSWORD=${{secrets.MYSQL_PASSWORD_FOR_JUICEDATA}} 
   sudo chmod +x .github/scripts/db.py
   version=$(./juicefs -V|cut -b 17- | sed 's/:/-/g')
   python3 .github/scripts/db.py --name load_big_dir --result $runtime --version $version --meta ${{matrix.meta}} --storage file
   start=`date +%s`
-  ./juicefs dump $META_URL dump.json
+  if [ "$with_subdir" = true ] ; then
+    ./juicefs dump $META_URL dump.json --subdir test
+  else
+    ./juicefs dump $META_URL dump.json
+  fi
   end=`date +%s`
   runtime=$((end-start))
   echo "dump cost $runtime seconds"
@@ -91,7 +102,6 @@ test_list_with_big_dir(){
     exit 1
   fi
 }
-
 
 prepare_test()
 {
