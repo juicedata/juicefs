9077586fa8b9ee064c8609c8c9ecd6936926d0fd
zhijian <djx@juicedata.io>
2023-07-13 16:00:40 +0800

    Limit the maximum resolve depth to prevent infinite loops (#3896)
    * Limit the maximum resolve depth to prevent infinite loops  Co-authored-by: Cheng Zhou <zhoucheng@juicedata.io>

--- .github/scripts/command/sync_minio.sh
+++ .github/scripts/command/sync_minio.sh
@@ -95,13 +95,32 @@ test_sync_external_link(){
     [ -z $(./mc cat myminio/myjfs/hello) ]
 }
 
-skip_test_sync_broken_link(){
+test_sync_loop_symlink(){
     prepare_test
     touch hello
     ln -s hello /jfs/hello
-    ./juicefs sync minio://minioadmin:minioadmin@localhost:9005/myjfs/ myjfs/ && exit 1 || true
+    ./juicefs sync minio://minioadmin:minioadmin@localhost:9005/myjfs/ minio://minioadmin:minioadmin@localhost:9000/myjfs/ && exit 1 || true
     rm -rf /jfs/hello
-    ./juicefs sync minio://minioadmin:minioadmin@localhost:9005/myjfs/ myjfs/
+    ./juicefs sync minio://minioadmin:minioadmin@localhost:9005/myjfs/ minio://minioadmin:minioadmin@localhost:9000/myjfs/
+}
+
+test_sync_deep_symlink(){
+    prepare_test
+    cd /jfs
+    echo hello > hello
+    ln -s hello symlink_1
+    for i in {1..40}; do
+        ln -s symlink_$i symlink_$((i+1))
+    done
+    cat symlink_40 | grep hello
+    cat symlink_41 && echo "cat symlink_41 fail" && exit 1 || true
+    cd -
+    ./juicefs sync minio://minioadmin:minioadmin@localhost:9005/myjfs/ minio://minioadmin:minioadmin@localhost:9000/myjfs/ && echo "sync should fail" && exit 1 || true
+    rm -rf /jfs/symlink_41
+    ./juicefs sync minio://minioadmin:minioadmin@localhost:9005/myjfs/ minio://minioadmin:minioadmin@localhost:9000/myjfs/ 
+    for i in {1..40}; do
+        ./mc cat myminio/myjfs/symlink_$i | grep "^hello$"
+    done
 }
 
 prepare_test(){
