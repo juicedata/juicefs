96f065f437fabec3e6fa739486915ff79fe4c6bc
Cheng Zhou <zhoucheng@juicedata.io>
2023-07-19 14:13:10 +0800

    wip

--- .github/scripts/command/sync_cluster.sh
+++ .github/scripts/command/sync_cluster.sh
@@ -6,33 +6,30 @@ source .github/scripts/start_meta_engine.sh
 start_meta_engine $META
 META_URL=$(get_meta_url $META)
 
-rm ~/.ssh/myKey -rf 
-ssh-keygen -t ed25519 -C "myKey" -f ~/.ssh/myKey -q -N ""
-cp -f ~/.ssh/myKey.pub .github/scripts/ssh/myKey.pub
+rm ~/.ssh/id_rsa -rf 
+rm ~/.ssh/id_rsa.pub -rf 
+ssh-keygen -t ed25519 -C "default" -f ~/.ssh/id_rsa -q -N ""
+cp -f ~/.ssh/id_rsa.pub .github/scripts/ssh/id_rsa.pub
 docker build -t juicedata/ssh -f .github/scripts/ssh/Dockerfile .github/scripts/ssh
 
 docker compose -f .github/scripts/ssh/docker-compose.yml up -d
 
 test_sync_with_mount_point(){
     do_sync_with_mount_point 
-    do_sync_with_mount_point --list-threads 10 --list-depth 5
-    do_sync_with_mount_point --dirs --update --perms --check-all 
-    do_sync_with_mount_point --dirs --update --perms --check-all --list-threads 10 --list-depth 5
+    # do_sync_with_mount_point --list-threads 10 --list-depth 5
+    # do_sync_with_mount_point --dirs --update --perms --check-all 
+    # do_sync_with_mount_point --dirs --update --perms --check-all --list-threads 10 --list-depth 5
 }
 
 do_sync_with_mount_point(){
     prepare_test
     options=$@
-    generate_source_dir
-    ./juicefs format $META_URL myjfs
-    ./juicefs mount -d $META_URL /jfs
-    ./juicefs sync jfs_source/ /jfs/jfs_source/ --manager 172.20.0.1 --worker 172.20.0.2,172.20.0.3
-
-    if [[ ! "$options" =~ "--dirs" ]]; then
-        find jfs_source -type d -empty -delete
-    fi
-    find /jfs/jfs_source -type f -name ".*.tmp*" -delete
-    diff -ur --no-dereference jfs_source/ /jfs/jfs_source/
+    dd if=/dev/urandom of=/tmp/bigfile bs=1M count=1024
+    cp /tmp/bigfile /jfs/bigfile
+    ./juicefs sync minio://minioadmin:minioadmin@localhost:9005/myjfs/ \
+         minio://minioadmin:minioadmin@localhost:9000/myjfs/ \
+        --manager 172.20.0.1:8081 --worker 172.20.0.2,172.20.0.3
+    mc ls myminio/myjfs -r
 }
 
 test_sync_without_mount_point(){
@@ -58,19 +55,17 @@ do_sync_without_mount_point(){
     diff -ur --no-dereference  jfs_source/ /jfs/jfs_source
 }
 
-
-generate_source_dir(){
-    [[ ! -d jfs_source ]] && git clone https://github.com/juicedata/juicefs.git jfs_source
-    [[ -d jfs_source/empty_dir ]] && rm jfs_source/empty_dir -rf
-    chmod 777 jfs_source
-    mkdir jfs_source/empty_dir
-    dd if=/dev/urandom of=jfs_source/file bs=5M count=1
-    chmod 777 jfs_source/file
-    ln -sf file jfs_source/symlink_to_file
-    ln -f jfs_source/file jfs_source/hard_link_to_file
-    id -u juicefs  && sudo userdel juicefs
-    sudo useradd -u 1101 juicefs
-    sudo -u juicefs touch jfs_source/file2
+prepare_test(){
+    umount_jfs /jfs $META_URL
+    python3 .github/scripts/flush_meta.py $META_URL
+    rm -rf /var/jfs/myjfs
+    rm -rf /var/jfsCache/myjfs
+    (./mc rb myminio/myjfs > /dev/null 2>&1 --force || true) && ./mc mb myminio/myjfs
+    ./juicefs format $META_URL myjfs
+    ./juicefs mount -d $META_URL /jfs
+    lsof -i :9005 | awk 'NR!=1 {print $2}' | xargs -r kill -9
+    MINIO_ROOT_USER=minioadmin MINIO_ROOT_PASSWORD=minioadmin ./juicefs gateway $META_URL localhost:9005 &
+    ./mc alias set juicegw http://localhost:9005 minioadmin minioadmin --api S3v4
 }
 
 
