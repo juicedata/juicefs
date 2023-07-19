1f84aef2b5cc92f2fbdf674a90f02274b7887664
Cheng Zhou <zhoucheng@juicedata.io>
2023-07-19 15:30:33 +0800

    wip

--- .github/scripts/command/sync_cluster.sh
+++ .github/scripts/command/sync_cluster.sh
@@ -30,45 +30,40 @@ docker build -t juicedata/ssh -f .github/scripts/ssh/Dockerfile .github/scripts/
 
 docker compose -f .github/scripts/ssh/docker-compose.yml up -d
 
-test_sync_with_mount_point(){
-    do_sync_with_mount_point 
-    # do_sync_with_mount_point --list-threads 10 --list-depth 5
-    # do_sync_with_mount_point --dirs --update --perms --check-all 
-    # do_sync_with_mount_point --dirs --update --perms --check-all --list-threads 10 --list-depth 5
+test_sync_small_files(){
+    prepare_test
+    ./juicefs mdtest $META_URL /test --dirs 10 --depth 3 --files 5 --threads 10
+    ./juicefs sync minio://minioadmin:minioadmin@localhost:9005/myjfs/ \
+         minio://minioadmin:minioadmin@localhost:9000/myjfs/ \
+        --manager 172.20.0.1:8081 --worker 172.20.0.2,172.20.0.3 --list-threads 10 --list-depth 5
+    ./mc ls myminio/myjfs/test -r |wc -l
+    ./mc ls juicegw/myjfs/test -r |wc -l
 }
 
-do_sync_with_mount_point(){
+test_sync_big_file(){
     prepare_test
-    options=$@
     dd if=/dev/urandom of=/tmp/bigfile bs=1M count=1024
     cp /tmp/bigfile /jfs/bigfile
     ./juicefs sync minio://minioadmin:minioadmin@localhost:9005/myjfs/ \
          minio://minioadmin:minioadmin@localhost:9000/myjfs/ \
         --manager 172.20.0.1:8081 --worker 172.20.0.2,172.20.0.3
-    ./mc ls myminio/myjfs -r
+    ./mc cat myminio/myjfs/bigfile | md5sum
+    cat /tmp/bigfile | md5sum
 }
 
 test_sync_without_mount_point(){
-    exit 0
-    # do_sync_without_mount_point 
-    # do_sync_without_mount_point --list-threads 10 --list-depth 5
-    # do_sync_without_mount_point --dirs --update --perms --check-all 
-    # do_sync_without_mount_point --dirs --update --perms --check-all --list-threads 10 --list-depth 5
-}
+    umount_jfs /jfs $META_URL
+    python3 .github/scripts/flush_meta.py $META_URL
+    rm -rf /var/jfs/myjfs
+    rm -rf /var/jfsCache/myjfs
+    (./mc rb myminio/myjfs > /dev/null 2>&1 --force || true) && ./mc mb myminio/myjfs
 
-do_sync_without_mount_point(){
-    prepare_test
-    options=$@
-    generate_source_dir
+    python3 .github/scripts/fsrand.py -c 1000 fsrand -v -a
     ./juicefs format $META_URL myjfs
-    meta_url=$META_URL ./juicefs sync jfs_source/ jfs://meta_url/jfs_source/ $options --links
-
+    meta_url=$META_URL ./juicefs sync fsrand/ jfs://meta_url/fsrand/ \
+         --manager 172.20.0.1:8081 --worker 172.20.0.2,172.20.0.3
     ./juicefs mount -d $META_URL /jfs
-    if [[ ! "$options" =~ "--dirs" ]]; then
-        find jfs_source -type d -empty -delete
-    fi
-    find /jfs/jfs_source -type f -name ".*.tmp*" -delete
-    diff -ur --no-dereference  jfs_source/ /jfs/jfs_source
+    diff -ur --no-dereference  fsrand/ /jfs/fsrand
 }
 
 prepare_test(){
