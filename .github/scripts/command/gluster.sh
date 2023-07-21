2be73d7384afb045dfe4570bb5af3da8d5a0f983
Cheng Zhou <zhoucheng@juicedata.io>
2023-07-21 13:26:47 +0800

    wip

--- .github/scripts/command/gluster.sh
+++ .github/scripts/command/gluster.sh
@@ -15,11 +15,12 @@ test_gluster()
     echo "Sleep 10 seconds to wait the glusterfs up"
     sleep 10
     docker compose exec glusterfs1 gluster peer probe glusterfs2
-    docker compose exec glusterfs1 gluster volume create test-volume replica 2 transport tcp glusterfs1:/data/glusterfs/test glusterfs2:/data/glusterfs/test
+    docker compose exec glusterfs1 gluster volume create test-volume replica 2 transport tcp glusterfs1:/data/glusterfs/test glusterfs2:/data/glusterfs/test force
     docker compose exec glusterfs1 gluster volume start test-volume
     docker compose exec glusterfs1 setfacl -m u:1000:rwx /data/glusterfs/test
     docker compose exec glusterfs2 setfacl -m u:1000:rwx /data/glusterfs/test
-    docker compose exec glusterfs1 tail -f /var/log/glusterfs/bricks/data-glusterfs-test.log
+    docker compose exec glusterfs1 cat /var/log/glusterfs/bricks/data-glusterfs-test.log
+    exit 1
 }
 
 
