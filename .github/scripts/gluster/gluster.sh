53ab31126ecddc154212b139853b225e8cc08be6
Cheng Zhou <zhoucheng@juicedata.io>
2023-07-24 19:54:40 +0800

    wip

--- .github/scripts/gluster/gluster.sh
+++ .github/scripts/gluster/gluster.sh
@@ -8,7 +8,6 @@ META_URL=$(get_meta_url $META)
 
 pkg -s glusterfs-server || .github/scripts/apt_install.sh glusterfs-server
 systemctl start glusterd.service
-
 mkdir -p /data/brick/gv0
 ip=$(ifconfig eth0 | grep 'inet ' |  awk '{ print $2 }')
 echo ip is $ip
@@ -16,11 +15,12 @@ gluster volume create gv0 $ip:/data/brick/gv0 force
 gluster volume start gv0
 gluster volume info
 
-./juicefs format $META_URL glusterfs-test --storage gluster --bucket $ip/gv0 
-./juicefs mount $META_URL /jfs -d
-echo abc > /jfs/abc
-cat /jfs/abc | grep abc
-
+test_gluster(){
+    ./juicefs format $META_URL glusterfs-test --storage gluster --bucket $ip/gv0 
+    ./juicefs mount $META_URL /jfs -d
+    echo abc > /jfs/abc
+    cat /jfs/abc | grep abc
+}
 
 source .github/scripts/common/run_test.sh && run_test $@
 
