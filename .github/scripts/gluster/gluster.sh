f49f06829b2119f2b0a730d2fee5e39f39aeffd5
Cheng Zhou <zhoucheng@juicedata.io>
2023-07-24 16:56:03 +0800

    wip

--- .github/scripts/gluster/gluster.sh
+++ .github/scripts/gluster/gluster.sh
@@ -6,19 +6,31 @@ source .github/scripts/start_meta_engine.sh
 start_meta_engine $META
 META_URL=$(get_meta_url $META)
 
-test_gluster()
+pkg -s glusterfs-server || .github/scripts/apt_install.sh glusterfs-server
+systemctl start glusterd.service
+
+mkdir -p /data/brick/gv0
+gluster volume create gv0 replica localhost:/data/brick/gv0
+gluster volume start gv0
+gluster volume info
+
+./juicefs.gluster format $META_URL glusterfs-test --storage gluster --bucket localhost/gv0
+
+skip_install_gluster_with_docker()
 {
     cd .github/scripts/gluster
     docker compose up -d glusterfs1 glusterfs2 glusters3
     echo "Sleep 10 seconds to wait the glusterfs up"
     sleep 10
     docker compose exec glusterfs1 gluster peer probe glusterfs2
-    docker compose exec glusterfs1 gluster volume create test-volume replica 2 transport tcp glusterfs1:/data/glusterfs/test glusterfs2:/data/glusterfs/test force
+    docker compose exec glusterfs1 gluster volume create test-volume replica 2 transport tcp \
+        glusterfs1:/data/glusterfs/test \
+        glusterfs2:/data/glusterfs/test force
     docker compose exec glusterfs1 gluster volume start test-volume
     docker compose exec glusterfs1 setfacl -m u:1000:rwx /data/glusterfs/test
     docker compose exec glusterfs2 setfacl -m u:1000:rwx /data/glusterfs/test
     docker compose exec glusterfs1 cat /var/log/glusterfs/bricks/data-glusterfs-test.log
-    ./juicefs format $META_URL gfs-test --storage gluster --bucket glusterfs1,glusterfs2/test-volume
+    ./juicefs.gluster format $META_URL gfs-test --storage gluster --bucket glusterfs1,glusterfs2/test-volume
 }
 
 
