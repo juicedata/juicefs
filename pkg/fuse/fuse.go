668c6d48655ec0ec381bf42672de50132231b44a
Sandy Xu <sandy@juicedata.io>
2023-07-26 10:48:51 +0800

    vfs: fix ioctl result and support FS_IOC_FSGETXATTR cmd

--- pkg/fuse/fuse.go
+++ pkg/fuse/fuse.go
@@ -421,7 +421,7 @@ func (fs *fileSystem) StatFs(cancel <-chan struct{}, in *fuse.InHeader, out *fus
 func (fs *fileSystem) Ioctl(cancel <-chan struct{}, in *fuse.IoctlIn, out *fuse.IoctlOut, bufIn, bufOut []byte) (status fuse.Status) {
 	ctx := fs.newContext(cancel, &in.InHeader)
 	defer releaseContext(ctx)
-	out.Result = int32(fs.v.Ioctl(ctx, Ino(in.NodeId), in.Cmd, in.Arg, bufIn, bufOut))
+	out.Result = -int32(fs.v.Ioctl(ctx, Ino(in.NodeId), in.Cmd, in.Arg, bufIn, bufOut))
 	return 0
 }
 
