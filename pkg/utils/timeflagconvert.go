883fe92fab71fdc60d2605dddd58dbd06c56cd3b
Zhoulin <443619637@qq.com>
2023-07-28 17:37:28 +0800

    delete chinese annotations

--- pkg/utils/timeflagconvert.go
+++ pkg/utils/timeflagconvert.go
@@ -10,10 +10,8 @@ import (
 func UploadDelayTimeLimtFlags(startuploadStr, enduploadStr string) (startHour, startMinute, endHour, endMinute int) {
 	var err error
 	if startuploadStr != "" && enduploadStr != "" {
-		//计算出新的delay时间
 		startParts := strings.Split(startuploadStr, ":")
 		endParts := strings.Split(enduploadStr, ":")
-		//获取到限制上传的开始时间
 		if len(startParts) == 1 {
 			startHour, err = strconv.Atoi(startParts[0])
 			if err != nil {
@@ -23,14 +21,13 @@ func UploadDelayTimeLimtFlags(startuploadStr, enduploadStr string) (startHour, s
 		} else {
 			startHour, err = strconv.Atoi(startParts[0])
 			if err != nil {
-				fmt.Println("请传入正确的时间段参数。")
+				fmt.Println("Please pass in the correct time period parameters.")
 			}
 			startMinute, err = strconv.Atoi(startParts[1])
 			if err != nil {
 				fmt.Println("Please pass in the correct time period parameters.")
 			}
 		}
-		//获取到限制上传的结束时间
 		if len(endParts) == 1 {
 			endHour, err = strconv.Atoi(endParts[0])
 			if err != nil {
