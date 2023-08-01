048bda9cf437935a659823711f06186bbb943dbd
Zhoulin <443619637@qq.com>
2023-08-01 17:19:47 +0800

    delete fmt.println use logger.Warnf

--- pkg/utils/timeflagconvert.go
+++ pkg/utils/timeflagconvert.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 )
@@ -15,33 +14,33 @@ func UploadDelayTimeLimtFlags(startuploadStr, enduploadStr string) (startHour, s
 		if len(startParts) == 1 {
 			startHour, err = strconv.Atoi(startParts[0])
 			if err != nil {
-				fmt.Println("Please pass in the correct time period parameters.")
+				logger.Warnf("Please pass in the correct time period parameters.")
 			}
 			startMinute = 0
 		} else {
 			startHour, err = strconv.Atoi(startParts[0])
 			if err != nil {
-				fmt.Println("Please pass in the correct time period parameters.")
+				logger.Warnf("Please pass in the correct time period parameters.")
 			}
 			startMinute, err = strconv.Atoi(startParts[1])
 			if err != nil {
-				fmt.Println("Please pass in the correct time period parameters.")
+				logger.Warnf("Please pass in the correct time period parameters.")
 			}
 		}
 		if len(endParts) == 1 {
 			endHour, err = strconv.Atoi(endParts[0])
 			if err != nil {
-				fmt.Println("Please pass in the correct time period parameters.")
+				logger.Warnf("Please pass in the correct time period parameters.")
 			}
 			endMinute = 0
 		} else {
 			endHour, err = strconv.Atoi(endParts[0])
 			if err != nil {
-				fmt.Println("Please pass in the correct time period parameters.")
+				logger.Warnf("Please pass in the correct time period parameters.")
 			}
 			endMinute, err = strconv.Atoi(endParts[1])
 			if err != nil {
-				fmt.Println("Please pass in the correct time period parameters.")
+				logger.Warnf("Please pass in the correct time period parameters.")
 			}
 		}
 	}
