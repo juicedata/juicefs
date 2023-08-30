21bfd6219c5321917f491caa7c2bf9a9780fa227
wangzhaohui <wzh_allright@126.com>
2023-08-30 20:08:31 +0800

    sdk/java: Fix print result inaccurate when TestDFSIO by local

--- sdk/java/src/main/java/io/juicefs/bench/TestDFSIO.java
+++ sdk/java/src/main/java/io/juicefs/bench/TestDFSIO.java
@@ -312,7 +312,7 @@ public class TestDFSIO extends Main.Command {
             "Number files per thread: " + filesPerThread,
             "            Total files: " + threadsPerMap * filesPerThread,
             " Total MBytes processed: " + df.format(TestDFSIO.toMB(sizeProcessed.get())),
-            "Total Throughput mb/sec: " + df.format(TestDFSIO.toMB(sizeProcessed.get()) / TestDFSIO.msToSecs(end - start)),
+            "Total Throughput MB/sec: " + df.format(TestDFSIO.toMB(sizeProcessed.get()) / TestDFSIO.msToSecs(end - start)),
             "     Test exec time sec: " + df.format(TestDFSIO.msToSecs(end - start)),
             ""};
 
