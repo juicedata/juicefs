4d8cd8d796a1e771a03056be35dd176648d7a0de
zhijian <djx@juicedata.io>
2023-09-08 00:32:57 +0800

    disable compression

--- pkg/gateway/gateway.go
+++ pkg/gateway/gateway.go
@@ -77,7 +77,7 @@ type jfsObjects struct {
 }
 
 func (n *jfsObjects) IsCompressionSupported() bool {
-	return n.conf.Chunk.Compress != "" && n.conf.Chunk.Compress != "none"
+	return false
 }
 
 func (n *jfsObjects) IsEncryptionSupported() bool {
