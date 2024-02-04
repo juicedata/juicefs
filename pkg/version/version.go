96206c66ff02e9cf673b6973bdc99616b994aef5
Sandy Xu <sandy@juicedata.io>
2024-02-04 16:25:29 +0800

    bump to release 1.0.7

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -29,7 +29,7 @@ var (
 	ver          = Semver{
 		major:      1,
 		minor:      0,
-		patch:      6,
+		patch:      7,
 		preRelease: "",
 		build:      fmt.Sprintf("%s.%s", revisionDate, revision),
 	}
--- sdk/java/src/main/java/io/juicefs/JuiceFileSystemImpl.java
+++ sdk/java/src/main/java/io/juicefs/JuiceFileSystemImpl.java
@@ -509,7 +509,7 @@ public class JuiceFileSystemImpl extends FileSystem {
     LibraryLoader<Libjfs> libjfsLibraryLoader = LibraryLoader.create(Libjfs.class);
     libjfsLibraryLoader.failImmediately();
 
-    int soVer = 15;
+    int soVer = 17;
     String osId = "so";
     String archId = "amd64";
     String resourceFormat = "libjfs-%s.%s.gz";
