f0ce5ce23fd70f59e1ac1810bf775679614ed935
zhijian <djx@juicedata.io>
2024-02-26 18:12:16 +0800

    Revert "fix total numbre of objects in cluster mode"
    This reverts commit 1e1d11cf757db8d0365be75df2d4afd15da465f1.

--- pkg/sync/cluster.go
+++ pkg/sync/cluster.go
@@ -141,7 +141,6 @@ func startManager(config *Config, tasks <-chan object.Object) (string, error) {
 				if obj == nil {
 					break LOOP
 				}
-				handled.IncrTotal(1)
 				objs = append(objs, obj)
 				if len(objs) > 100 {
 					break LOOP
