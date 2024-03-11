c4a7bd97b5069b14511fbedc9470665ecab2ded1
zhijian <djx@juicedata.io>
2024-03-11 17:54:00 +0800

    metrics: export meta backup metrics (#4480)

--- cmd/mount.go
+++ cmd/mount.go
@@ -360,6 +360,8 @@ func initBackgroundTasks(c *cli.Context, vfsConf *vfs.Config, metaConf *meta.Con
 		vfsConf.Port.ConsulAddr = c.String("consul")
 	}
 	if !metaConf.ReadOnly && !metaConf.NoBGJob && vfsConf.BackupMeta > 0 {
+		registerer.MustRegister(vfs.LastBackupTimeG)
+		registerer.MustRegister(vfs.LastBackupDurationG)
 		go vfs.Backup(m, blob, vfsConf.BackupMeta)
 	}
 	if !c.Bool("no-usage-report") {
