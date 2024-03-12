15d76b511510f52ba25c84eaf9c6d8ebc98ddbe6
Zhou Cheng <zhoucheng@juicedata.io>
2024-03-12 18:52:31 +0800

    CI: Test acl (#4485)

--- .github/scripts/hypo/fsrand2_test.py
+++ .github/scripts/hypo/fsrand2_test.py
@@ -95,6 +95,16 @@ class TestFsrand2(unittest.TestCase):
         state.listdir(dir=v1, user='user1')
         state.teardown()
 
+    def test_acl_1022(self):
+        # SEE https://github.com/juicedata/jfs/issues/1022
+        state = JuicefsMachine()
+        v1 = state.init_folders()
+        state.create_file(content=b'\xda\x07', file_name='lbca', mode='w', parent=v1, umask=103, user='root')
+        state.set_acl(default=False, entry=v1, group='user1', group_perm={'r', 'w'}, logical=False, mask={'r', 'w', 'x'}, not_recalc_mask=True, other_perm=set(), physical=True, recalc_mask=True, recursive=True, set_mask=True, sudo_user='root', user='root', user_perm={'r', 'w', 'x'})
+        state.chmod(entry=v1, mode=0o4004, user='root')
+        state.set_acl(default=True, entry=v1, group='group4', group_perm={'x'}, logical=False, mask={'w', 'x'}, not_recalc_mask=False, other_perm=set(), physical=True, recalc_mask=False, recursive=True, set_mask=True, sudo_user='user1', user='user2', user_perm=set())
+        state.teardown()
+
     def test_acl_1044(self):
         # SEE: https://github.com/juicedata/jfs/issues/1044
         state = JuicefsMachine()
@@ -112,6 +122,27 @@ class TestFsrand2(unittest.TestCase):
         v1 = state.init_folders()
         v3 = state.set_acl(default=True, entry=v1, group='root', group_perm=set(), logical=False, mask=set(), not_recalc_mask=False, other_perm=set(), physical=False, recalc_mask=True, recursive=True, set_mask=True, sudo_user='root', user='user1', user_perm={v1, 'r', 'w', 'x'})
         state.create_file(content=b'', file_name='afds', mode='w', parent=v1, umask=295, user='root')
+        state.teardown()
+
+    def test_acl_4472(self):
+        # SEE: https://github.com/juicedata/juicefs/issues/4472
+        state = JuicefsMachine()
+        v1 = state.init_folders()
+        v2 = state.create_file(content=b'', file_name='stsn', mode='x', parent=v1, umask=464, user='root')
+        v3 = state.set_acl(default=True, entry=v1, group='group4', group_perm={'x'}, logical=False, mask={'w'}, not_recalc_mask=False, other_perm=set(), physical=False, recalc_mask=True, recursive=True, set_mask=True, sudo_user='root', user='root', user_perm={'r'})
+        v8 = state.create_file(content=b'', file_name='qpyt', mode='w', parent=v1, umask=233, user='root')
+        v9 = state.copy_file(entry=v2, follow_symlinks=False, new_entry_name='knmh', parent=v1, umask=23, user='root')
+        state.open(file=v8, flags=[512], mode=2579, umask=34, user='root')
+        state.teardown()
+
+    def test_acl_4483(self):
+        # SEE https://github.com/juicedata/juicefs/issues/4483
+        state = JuicefsMachine()
+        v1 = state.init_folders()
+        state.set_acl(default=True, entry=v1, group='root', group_perm={'r'}, logical=True, mask={'r'}, not_recalc_mask=False, other_perm={'r', 'x'}, physical=False, recalc_mask=False, recursive=True, set_mask=True, sudo_user='user1', user='user2', user_perm={'r', 'w', 'x'})
+        v4 = state.create_file(content=b'\xe65', file_name='abha', mode='a', parent=v1, umask=3, user='root')
+        v5 = state.set_acl(default=False, entry=v4, group='user3', group_perm={'x'}, logical=False, mask={'x'}, not_recalc_mask=True, other_perm=set(), physical=True, recalc_mask=True, recursive=False, set_mask=False, sudo_user='root', user='user1', user_perm=set())
+        state.list_xattr(file=v4, user='root')
         state.teardown()
 
 if __name__ == '__main__':
