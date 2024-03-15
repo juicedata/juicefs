55b083800df908b288af6ad163948cc6819902d7
Cheng Zhou <zhoucheng@juicedata.io>
2024-03-15 15:47:51 +0800

    wip

--- .github/scripts/command/acl.sh
+++ .github/scripts/command/acl.sh
@@ -18,7 +18,7 @@ test_acl_with_user_space_check()
 {
     prepare_test
     ./juicefs format $META_URL myjfs --enable-acl --trash-days 0
-    ./juicefs mount -d $META_URL /tmp/jfs --no-default-permission
+    ./juicefs mount -d $META_URL /tmp/jfs --non-default-permission
     python3 .github/scripts/hypo/acl_test.py 
 }
 
