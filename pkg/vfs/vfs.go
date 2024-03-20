af58e471d75c22ce019047450cd2de41af9abbcd
zhijian <djx@juicedata.io>
2024-03-20 10:42:06 +0800

    fix

--- pkg/vfs/vfs.go
+++ pkg/vfs/vfs.go
@@ -730,7 +730,7 @@ func (v *VFS) Read(ctx Context, ino Ino, buf []byte, off uint64, fh uint64) (n i
 
 	// there could be read operation for write-only if kernel writeback is enabled
 	if !v.Conf.FuseOpts.EnableWriteback && !hasReadPerm(h.flags) {
-		err = syscall.EACCES
+		err = syscall.EBADF
 		return
 	}
 	if !h.Rlock(ctx) {
