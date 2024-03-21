02fb578803815ee5475b0a87ec33fa1cf5935833
Sandy Xu <sandy@juicedata.io>
2024-03-21 23:15:54 +0800

    update "block-size" in format command

--- cmd/format.go
+++ cmd/format.go
@@ -35,6 +35,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/dustin/go-humanize"
 	"github.com/google/uuid"
 	"github.com/juicedata/juicefs/pkg/compress"
 	"github.com/juicedata/juicefs/pkg/meta"
@@ -143,9 +144,9 @@ func formatStorageFlags() []cli.Flag {
 
 func formatFlags() []cli.Flag {
 	return addCategories("DATA FORMAT", []cli.Flag{
-		&cli.IntFlag{
+		&cli.StringFlag{
 			Name:  "block-size",
-			Value: 4096,
+			Value: "4M",
 			Usage: "size of block in KiB",
 		},
 		&cli.StringFlag{
@@ -168,7 +169,6 @@ func formatFlags() []cli.Flag {
 		},
 		&cli.IntFlag{
 			Name:  "shards",
-			Value: 0,
 			Usage: "store the blocks into N buckets by hash of key",
 		},
 	})
@@ -199,8 +199,8 @@ func formatManagementFlags() []cli.Flag {
 	})
 }
 
-func fixObjectSize(s int) int {
-	const min, max = 64, 16 << 10
+func fixObjectSize(s uint64) uint64 {
+	const min, max = 64 << 10, 16 << 20
 	var bits uint
 	for s > 1 {
 		bits++
@@ -208,10 +208,10 @@ func fixObjectSize(s int) int {
 	}
 	s = s << bits
 	if s < min {
-		logger.Warnf("block size is too small: %d, use %d instead", s, min)
+		logger.Warnf("block size is too small: %s, use %s instead", humanize.IBytes(s), humanize.IBytes(min))
 		s = min
 	} else if s > max {
-		logger.Warnf("block size is too large: %d, use %d instead", s, max)
+		logger.Warnf("block size is too large: %s, use %s instead", humanize.IBytes(s), humanize.IBytes(max))
 		s = max
 	}
 	return s
@@ -405,7 +405,7 @@ func format(c *cli.Context) error {
 			case "trash-days":
 				format.TrashDays = c.Int(flag)
 			case "block-size":
-				format.BlockSize = fixObjectSize(c.Int(flag))
+				format.BlockSize = int(fixObjectSize(parseBytes(c, flag, 'K')) >> 10)
 			case "compress":
 				format.Compression = c.String(flag)
 			case "shards":
@@ -435,7 +435,7 @@ func format(c *cli.Context) error {
 			HashPrefix:       c.Bool("hash-prefix"),
 			Capacity:         c.Uint64("capacity") << 30,
 			Inodes:           c.Uint64("inodes"),
-			BlockSize:        fixObjectSize(c.Int("block-size")),
+			BlockSize:        int(fixObjectSize(parseBytes(c, "block-size", 'K')) >> 10),
 			Compression:      c.String("compress"),
 			TrashDays:        c.Int("trash-days"),
 			DirStats:         true,
