688dbae76b21f178c6ade6708966ec1250bb9714
Zhou Cheng <zhoucheng@juicedata.io>
2024-03-28 17:17:14 +0800

    CI: fix dump test (#4568)
    Co-authored-by: zhijian <djx@juicedata.io>

--- .github/scripts/command/format.sh
+++ .github/scripts/command/format.sh
@@ -9,25 +9,23 @@ META_URL=$(get_meta_url $META)
 test_mount_process_exit_on_format()
 {
     prepare_test
-    for i in {1..3}; do 
-        echo "round $i"
-        ./juicefs format $META_URL volume-$i
-        ./juicefs mount -d $META_URL /tmp/myjfs$i_$j --no-usage-report
-        cd /tmp/myjfs$i_$j
-        bash -c 'for k in {1..300}; do echo abc>$k; sleep 0.2; done' || true & 
-        cd -
-        sleep 3
-        uuid=$(./juicefs status $META_URL | grep UUID | cut -d '"' -f 4) 
-        ./juicefs destroy --force $META_URL $uuid
-        ./juicefs format $META_URL new-volume-$i 
-        sleep 15   
-        ps -ef | grep juicefs
-        # TODO: fix the bug and remove the following line
-        # SEE https://github.com/juicedata/juicefs/issues/4534
-        # pidof juicefs && exit 1
-        uuid=$(./juicefs status $META_URL | grep UUID | cut -d '"' -f 4) 
-        ./juicefs destroy --force $META_URL $uuid
-    done
+    echo "round $i"
+    ./juicefs format $META_URL volume-$i
+    ./juicefs mount -d $META_URL /tmp/myjfs$i_$j --no-usage-report
+    cd /tmp/myjfs$i_$j
+    bash -c 'for k in {1..300}; do echo abc>$k; sleep 0.2; done' || true & 
+    cd -
+    sleep 3
+    uuid=$(./juicefs status $META_URL | grep UUID | cut -d '"' -f 4) 
+    ./juicefs destroy --force $META_URL $uuid
+    ./juicefs format $META_URL new-volume-$i 
+    sleep 15   
+    ps -ef | grep juicefs
+    # TODO: fix the bug and remove the following line
+    # SEE https://github.com/juicedata/juicefs/issues/4534
+    pidof juicefs && exit 1
+    uuid=$(./juicefs status $META_URL | grep UUID | cut -d '"' -f 4) 
+    ./juicefs destroy --force $META_URL $uuid
 }
 
 
--- .github/scripts/command/load_dump.sh
+++ .github/scripts/command/load_dump.sh
@@ -6,6 +6,8 @@ source .github/scripts/start_meta_engine.sh
 start_meta_engine $META
 META_URL=$(get_meta_url $META)
 [[ -z "$SEED" ]] && SEED=$(date +%s)
+# [[ -z "$SEED" ]] && SEED=1711594639
+
 trap "echo random seed is $SEED" EXIT
 
 if ! docker ps | grep -q minio; then
@@ -64,32 +66,55 @@ test_dump_without_keep_secret()
     cat /jfs/hello.txt | grep hello
 }
 
-test_dump_load_with_fsrand()
-{
+test_dump_load_with_trash_enable(){
+    do_dump_load_with_fsrand 1
+}
+test_dump_load_with_trash_disable(){
+    do_dump_load_with_fsrand 0
+}
+
+do_dump_load_with_fsrand(){
+    trash_days=$1
     prepare_test
-    ./juicefs format $META_URL myjfs --trash-days 0 --enable-acl
+    ./juicefs format $META_URL myjfs --trash-days $trash_days --enable-acl
     ./juicefs mount -d $META_URL /jfs --enable-xattr
-    SEED=$SEED MAX_EXAMPLE=100 STEP_COUNT=50 PROFILE=generate ROOT_DIR1=/jfs/fsrand ROOT_DIR2=/tmp/fsrand python3 .github/scripts/hypo/fsrand2.py || true
-    ./juicefs dump $META_URL dump.json --fast
+    SEED=$SEED LOG_LEVEL=WARNING MAX_EXAMPLE=30 STEP_COUNT=20 PROFILE=generate ROOT_DIR1=/jfs/fsrand ROOT_DIR2=/tmp/fsrand python3 .github/scripts/hypo/fsrand2.py || true
+    # find /jfs/fsrand -mindepth 1 -maxdepth 1 ! -name "syly" -exec rm -rf {} \; 
+    do_dump_load_and_compare 
+    do_dump_load_and_compare --fast
+    do_dump_load_and_compare --skip-trash
+    do_dump_load_and_compare --fast --skip-trash
+}
+
+do_dump_load_and_compare()
+{
+    option=$@
+    echo option is $option
+    ./juicefs dump $META_URL dump.json $option
+    rm -rf test2.db 
     ./juicefs load sqlite3://test2.db dump.json
-    ./juicefs dump sqlite3://test2.db dump2.json --fast
+    ./juicefs dump sqlite3://test2.db dump2.json $option
     # compare_dump_json
     ./juicefs mount -d sqlite3://test2.db /jfs2
     diff -ur /jfs/fsrand /jfs2/fsrand --no-dereference
-    compare_stat_acl /jfs/fsrand /jfs2/fsrand
+    compare_stat_acl_xattr /jfs/fsrand /jfs2/fsrand
+    umount /jfs2
 }
 
 compare_dump_json(){
-    sed -i '/usedSpace/d' dump*.json
-    sed -i '/usedInodes/d' dump*.json
-    sed -i '/nextInodes/d' dump*.json
-    sed -i '/nextChunk/d' dump*.json
-    sed -i '/nextSession/d' dump*.json
-    sed -i 's/"inode":[0-9]\+/"inode":0/g' dump*.json
-    diff -ur dump.json dump2.json
+    cp dump.json dump.json.bak
+    cp dump2.json dump2.json.bak
+    sed -i '/usedSpace/d' dump*.json.bak
+    sed -i '/usedInodes/d' dump*.json.bak
+    sed -i '/nextInodes/d' dump*.json.bak
+    sed -i '/nextChunk/d' dump*.json.bak
+    sed -i '/nextTrash/d' dump*.json.bak
+    sed -i '/nextSession/d' dump*.json.bak
+    sed -i 's/"inode":[0-9]\+/"inode":0/g' dump*.json.bak
+    diff -ur dump.json.bak dump2.json.bak
 }
 
-compare_stat_acl(){
+compare_stat_acl_xattr(){
     dir1=$1
     dir2=$2
     files1=($(find "$dir1" -type f -o -type d -exec stat -c "%n" {} + | sort))
@@ -100,8 +125,12 @@ compare_stat_acl(){
         stat2=$(stat -c "%F %a %s %h %U %G" "${files2[$i]}")
         acl1=$(getfacl -p "${files1[$i]}" | tail -n +2)
         acl2=$(getfacl -p "${files2[$i]}" | tail -n +2)
+        xattr1=$(getfattr -d -m . -e hex "${files1[$i]}" 2>/dev/null | tail -n +2 | sort)
+        xattr2=$(getfattr -d -m . -e hex "${files2[$i]}" 2>/dev/null | tail -n +2 | sort)
         [[ "$stat1" != "$stat2" ]] && echo "compare_stat_acl: stat for ${files1[$i]} and ${files2[$i]} differs" && echo $stat1 && echo $stat2 && exit 1
         [[ "$acl1" != "$acl2" ]] && echo "compare_stat_acl: ACLs for ${files1[$i]} and ${files2[$i]} differs" && echo $acl1 && echo $acl2 && exit 1
+        [[ "$xattr1" != "$xattr2" ]] && echo "compare_stat_acl: xattrs for ${files1[$i]} and ${files2[$i]} differs" && echo $xattr1 && echo $xattr2 && exit 1
+
     done
     echo "compare_stat_acl: ACLs and stats are the same"
 }
@@ -113,7 +142,7 @@ test_load_encrypted_meta_backup()
     export JFS_RSA_PASSPHRASE=12345678
     ./juicefs format $META_URL myjfs --encrypt-rsa-key my-priv-key.pem
     ./juicefs mount -d $META_URL /jfs
-    SEED=$SEED MAX_EXAMPLE=50 STEP_COUNT=50 PROFILE=generate ROOT_DIR1=/jfs/fsrand ROOT_DIR2=/tmp/fsrand python3 .github/scripts/hypo/fsrand2.py || true
+    SEED=$SEED LOG_LEVEL=WARNING MAX_EXAMPLE=50 STEP_COUNT=50 PROFILE=generate ROOT_DIR1=/jfs/fsrand ROOT_DIR2=/tmp/fsrand python3 .github/scripts/hypo/fsrand2.py || true
     umount /jfs
     SKIP_BACKUP_META_CHECK=true ./juicefs mount -d --backup-meta 10s $META_URL /jfs
     sleep 10s
