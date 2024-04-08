72de0333e3714166390e8575a905c6b74ef9e881
Zhou Cheng <zhoucheng@juicedata.io>
2024-04-08 13:32:41 +0800

    CI: Test gateway with random test. (#4643)

---
+++ .github/scripts/hypo/s3_contant.py
@@ -0,0 +1,6 @@
+ROOT_ALIAS = 'admin'
+ROOT_ACCESS_KEY = 'minioadmin'
+ROOT_SECRET_KEY = 'minioadmin'
+DEFAULT_ACCESS_KEY = ROOT_ACCESS_KEY
+DEFAULT_SECRET_KEY = ROOT_SECRET_KEY
+BUILD_IN_POLICIES = ('consoleAdmin', 'readonly', 'readwrite', 'writeonly')
