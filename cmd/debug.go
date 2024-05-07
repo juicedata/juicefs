89b162ab71089af7b2e98af2f7ca491633173fea
zhijian <djx@juicedata.io>
2024-05-07 17:37:40 +0800

    adjust

--- cmd/debug.go
+++ cmd/debug.go
@@ -122,21 +122,33 @@ func copyFile(srcPath, destPath string, requireRootPrivileges bool) error {
 }
 
 func getCmdMount(mp string) (uid, pid, cmd string, err error) {
-	content, err := readConfig(mp)
-	if err != nil {
-		logger.Warnf("failed to read config file: %v", err)
-	}
-	cfg := vfs.Config{}
-	if err := json.Unmarshal(content, &cfg); err != nil {
-		logger.Warnf("failed to unmarshal config file: %v", err)
-	}
+	_ = utils.WithTimeout(func() error {
+		content, err := readConfig(mp)
+		if err != nil {
+			logger.Warnf("failed to read config file: %v", err)
+		}
+		cfg := vfs.Config{}
+		if err := json.Unmarshal(content, &cfg); err != nil {
+			logger.Warnf("failed to unmarshal config file: %v", err)
+		}
+		if cfg.Pid != 0 {
+			pid = strconv.Itoa(cfg.Pid)
+		}
+		return nil
+	}, time.Second)
 
-	psArgs := []string{"/bin/sh", "-c", fmt.Sprintf("ps -f --pid %d", cfg.Pid)}
+	var psArgs []string
+	if pid != "" {
+		psArgs = []string{"/bin/sh", "-c", fmt.Sprintf("ps -f --pid %s", pid)}
+	} else {
+		psArgs = []string{"/bin/sh", "-c", fmt.Sprintf("ps -ef | grep -v grep | grep mount | grep %s", mp)}
+	}
 	ret, err := exec.Command(psArgs[0], psArgs[1:]...).CombinedOutput()
 	if err != nil {
 		return "", "", "", fmt.Errorf("failed to execute command `%s`: %v", strings.Join(psArgs, " "), err)
 	}
 	var find bool
+	var ppid string
 	lines := strings.Split(string(ret), "\n")
 	for i := len(lines) - 1; i >= 0; i-- {
 		line := lines[i]
@@ -148,10 +160,18 @@ func getCmdMount(mp string) (uid, pid, cmd string, err error) {
 		for _, arg := range cmdFields {
 			if mp == arg {
 				if find {
-					return "", "", "", fmt.Errorf("find more than one mount process for %s", mp)
+					newCmd := strings.Join(fields[7:], " ")
+					newUid, newPid, newPpid := strings.TrimSpace(fields[0]), strings.TrimSpace(fields[1]), strings.TrimSpace(fields[2])
+					if newPid == ppid {
+						return uid, pid, cmd, nil
+					} else if pid == newPpid {
+						return newUid, newPid, newCmd, nil
+					} else {
+						return "", "", "", fmt.Errorf("find more than one mount process for %s", mp)
+					}
 				}
 				cmd = strings.Join(fields[7:], " ")
-				uid, _ = strings.TrimSpace(fields[0]), strings.TrimSpace(fields[1])
+				uid, pid, ppid = strings.TrimSpace(fields[0]), strings.TrimSpace(fields[1]), strings.TrimSpace(fields[2])
 				find = true
 			}
 		}
@@ -159,7 +179,7 @@ func getCmdMount(mp string) (uid, pid, cmd string, err error) {
 	if cmd == "" {
 		return "", "", "", fmt.Errorf("no mount command found for %s", mp)
 	}
-	return uid, strconv.Itoa(cfg.Pid), cmd, nil
+	return uid, pid, cmd, nil
 }
 
 var logArg = regexp.MustCompile(`--log(\s*=?\s*)(\S+)`)
