57a603e140bb8c879dd34bd083f59d55e73db5b4
zhoucheng <zhoucheng@juicedata.io>
2024-05-28 20:01:19 +0800

    wip

--- .github/scripts/command/gateway2.sh
+++ .github/scripts/command/gateway2.sh
@@ -8,8 +8,12 @@ META_URL=$(get_meta_url $META)
 [[ ! -x /usr/local/bin/mc ]] && wget -q https://dl.min.io/client/mc/release/linux-amd64/archive/mc.RELEASE.2021-04-22T17-40-00Z -O /usr/local/bin/mc && sudo chmod +x /usr/local/bin/mc
 # docker ps -aq --filter "status=exited" --filter "name=minio_old" | xargs -r docker rm -v
 if ! docker ps --filter "name=minio_old$" | grep minio_old; then
+    echo start minio_old
     docker run -d -p 9004:9004 --name minio_old -e "MINIO_ACCESS_KEY=minioadmin" -e "MINIO_SECRET_KEY=minioadmin" minio/minio:RELEASE.2021-04-22T15-44-28Z server /tmp/minio_old
 fi
+
+timeout 30 bash -c 'counter=0; until lsof -i:9004; do echo -ne "wait port ready in $counter\r" && ((counter++)) && sleep 1; done'
+
 [[ -n $CI ]] && trap 'kill_gateway 9005;' EXIT
 kill_gateway() {
     port=$1
