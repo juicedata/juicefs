bbd6163b3f99f9b57c3088fc816b98e38ef5e3af
zhoucheng <zhoucheng@juicedata.io>
2024-05-28 18:04:19 +0800

    wip

---
+++ .github/scripts/command/gateway2.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e
+source .github/scripts/common/common.sh
+
+[[ -z "$META" ]] && META=redis
+source .github/scripts/start_meta_engine.sh
+start_meta_engine $META
+META_URL=$(get_meta_url $META)
+[[ ! -x /usr/local/bin/mc ]] && wget -q https://dl.min.io/client/mc/release/linux-amd64/archive/mc.RELEASE.2021-04-22T17-40-00Z -O /usr/local/bin/mc && sudo chmod +x /usr/local/bin/mc
+# docker ps -aq --filter "status=exited" --filter "name=minio_old" | xargs -r docker rm -v
+if ! docker ps --filter "name=minio_old$" | grep minio_old; then
+    docker run -d -p 9004:9004 --name minio_old -e "MINIO_ACCESS_KEY=minioadmin" -e "MINIO_SECRET_KEY=minioadmin" minio/minio:RELEASE.2021-04-22T15-44-28Z server /tmp/minio_old
+fi
+[[ -n $CI ]] && trap 'kill_gateway 9005;' EXIT
+kill_gateway() {
+    port=$1
+    lsof -i:$port || true
+    lsof -t -i :$port | xargs -r kill -9 || true
+}
+
+prepare_test()
+{
+    umount_jfs /tmp/jfs $META_URL
+    kill_gateway 9005
+    python3 .github/scripts/flush_meta.py $META_URL
+    rm -rf /var/jfs/myjfs || true
+    ./juicefs format $META_URL myjfs  --trash-days 0
+    # ./juicefs mount -d $META_URL /tmp/jfs
+    MINIO_ROOT_USER=minioadmin MINIO_ROOT_PASSWORD=minioadmin ./juicefs gateway $META_URL localhost:9005 --multi-buckets --keep-etag -d
+}
+
+test_run_example()
+{
+    python3 .github/scripts/hypo/s3_test.py
+}
+
+test_run_all()
+{
+    python3 .github/scripts/hypo/s3.py
+}
+
+
+
+
+source .github/scripts/common/run_test.sh && run_test $@
+
