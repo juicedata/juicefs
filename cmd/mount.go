ac063c20f39b5bb29f2e0a8785f4847dbb1e984f
zhijian <djx@juicedata.io>
2024-07-05 15:07:02 +0800

    backup: print a warning message that backups are not turned on

--- cmd/mount.go
+++ cmd/mount.go
@@ -382,6 +382,8 @@ func initBackgroundTasks(c *cli.Context, vfsConf *vfs.Config, metaConf *meta.Con
 		registerer.MustRegister(vfs.LastBackupTimeG)
 		registerer.MustRegister(vfs.LastBackupDurationG)
 		go vfs.Backup(m, blob, vfsConf.BackupMeta, vfsConf.BackupSkipTrash)
+	} else {
+		logger.Warnf("Backup meta is disabled")
 	}
 	if !c.Bool("no-usage-report") {
 		go usage.ReportUsage(m, version.Version())
