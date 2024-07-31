5cdf1c87ab9039577ef536a8c82fab5b40c7e964
jiefenghuang <jiefeng@juicedata.io>
2024-07-31 15:35:58 +0800

    fix: run on win
    Signed-off-by: jiefenghuang <jiefeng@juicedata.io>

--- cmd/mount.go
+++ cmd/mount.go
@@ -568,7 +568,7 @@ func mount(c *cli.Context) error {
 		logger.Fatalf("Invalid daemon stage: %d", stage)
 	}
 	supervisor := os.Getenv("JFS_SUPERVISOR")
-	if supervisor != "" {
+	if supervisor != "" || runtime.GOOS == "windows" {
 		stage = 3
 	}
 
