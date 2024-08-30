fc080fdbeda39be6ab86e59718edbc64a39cded6
Sandy Xu <sandy@juicedata.io>
2024-08-30 14:42:11 +0800

    bump to release 1.1.4

--- sdk/java/src/main/java/io/juicefs/JuiceFileSystemImpl.java
+++ sdk/java/src/main/java/io/juicefs/JuiceFileSystemImpl.java
@@ -573,7 +573,7 @@ public class JuiceFileSystemImpl extends FileSystem {
     LibraryLoader<Libjfs> libjfsLibraryLoader = LibraryLoader.create(Libjfs.class);
     libjfsLibraryLoader.failImmediately();
 
-    int soVer = 18;
+    int soVer = 20;
     String osId = "so";
     String archId = "amd64";
     String resourceFormat = "libjfs-%s.%s.gz";
