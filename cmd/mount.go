c90a175d323a4f4593b3eaff7c750d3611417b28
Changxin Miao <miaochangxin@step.ai>
2024-11-08 20:08:35 +0800

    Perform object storage test at startup (#5281)
    Signed-off-by: Changxin Miao <miaochangxin@step.ai>

--- cmd/mount.go
+++ cmd/mount.go
@@ -644,6 +644,16 @@ func mount(c *cli.Context) error {
 			}
 		}
 		if blob != nil {
+			// test storage at startup to fail fast instead of throwing EIO in the middle of user's workload
+			if c.Bool("check-storage") {
+				start := time.Now()
+				if err = test(blob); err != nil {
+					logger.Errorf("Object storage test failed: %s", err)
+					return err
+				} else {
+					logger.Infof("Object storage test passed in %s", time.Since(start))
+				}
+			}
 			object.Shutdown(blob)
 		}
 		var foreground bool
