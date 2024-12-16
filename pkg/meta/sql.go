66db3e0265a99aa3d08a524be2efed11eb6e3eee
Davies Liu <davies@juicedata.io>
2024-12-16 11:03:26 +0800

    don't reuse the session for readonly transaction

--- pkg/meta/sql.go
+++ pkg/meta/sql.go
@@ -856,8 +856,8 @@ func (m *dbMeta) roTxn(ctx context.Context, f func(s *xorm.Session) error) error
 	if v := ctx.Value(txSessionKey{}); v != nil {
 		s = v.(*xorm.Session)
 	} else {
-		s = m.spool.Get().(*xorm.Session)
-		defer m.spool.Put(s)
+		s = m.db.NewSession()
+		defer s.Close()
 	}
 	var opt sql.TxOptions
 	if !m.noReadOnlyTxn {
