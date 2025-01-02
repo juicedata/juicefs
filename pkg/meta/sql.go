4ce4facab52a449dc7fffa154cc54255c91d2de2
zhijian <djx@juicedata.io>
2025-01-02 16:15:52 +0800

    Revert "dump/sql: use total inodes to set the total number of the progress bar"
    This reverts commit b337ad63981510f56e6cd252aab89a95fb0d843c.

--- pkg/meta/sql.go
+++ pkg/meta/sql.go
@@ -3388,7 +3388,7 @@ func (m *dbMeta) doFlushQuotas(ctx Context, quotas map[Ino]*Quota) error {
 	})
 }
 
-func (m *dbMeta) dumpEntry(s *xorm.Session, inode Ino, typ uint8, e *DumpedEntry) error {
+func (m *dbMeta) dumpEntry(s *xorm.Session, inode Ino, typ uint8, e *DumpedEntry, showProgress func(totalIncr, currentIncr int64)) error {
 	n := &node{Inode: inode}
 	ok, err := s.Get(n)
 	if err != nil {
@@ -3464,6 +3464,9 @@ func (m *dbMeta) dumpEntry(s *xorm.Session, inode Ino, typ uint8, e *DumpedEntry
 		err := s.Limit(1000, 0).Find(&edges, &edge{Parent: inode})
 		if err != nil {
 			return err
+		}
+		if showProgress != nil {
+			showProgress(int64(len(edges)), 0)
 		}
 		if len(edges) < 1000 {
 			e.Entries = make(map[string]*DumpedEntry, len(edges))
@@ -3544,7 +3547,7 @@ func (m *dbMeta) dumpEntryFast(inode Ino, typ uint8) *DumpedEntry {
 	return e
 }
 
-func (m *dbMeta) dumpDir(s *xorm.Session, inode Ino, tree *DumpedEntry, bw *bufio.Writer, depth, threads int, bar *utils.Bar) error {
+func (m *dbMeta) dumpDir(s *xorm.Session, inode Ino, tree *DumpedEntry, bw *bufio.Writer, depth, threads int, showProgress func(totalIncr, currentIncr int64)) error {
 	bwWrite := func(s string) {
 		if _, err := bw.WriteString(s); err != nil {
 			panic(err)
@@ -3566,6 +3569,9 @@ func (m *dbMeta) dumpDir(s *xorm.Session, inode Ino, tree *DumpedEntry, bw *bufi
 			ce.Attr.Type = typeToString(edge.Type)
 			tree.Entries[name] = ce
 		}
+		if showProgress != nil {
+			showProgress(int64(len(edges))-1000, 0)
+		}
 	}
 	var entries []*DumpedEntry
 	for _, e := range tree.Entries {
@@ -3585,7 +3591,7 @@ func (m *dbMeta) dumpDir(s *xorm.Session, inode Ino, tree *DumpedEntry, bw *bufi
 				_ = m.roTxn(Background(), func(s *xorm.Session) error {
 					for i := c; i < len(entries) && err == nil; i += threads {
 						e := entries[i]
-						er := m.dumpEntry(s, e.Attr.Inode, 0, e)
+						er := m.dumpEntry(s, e.Attr.Inode, 0, e, showProgress)
 						ms[c].Lock()
 						ready[c] = true
 						if er != nil {
@@ -3616,7 +3622,7 @@ func (m *dbMeta) dumpDir(s *xorm.Session, inode Ino, tree *DumpedEntry, bw *bufi
 			return err
 		}
 		if e.Attr.Type == "directory" {
-			err = m.dumpDir(s, e.Attr.Inode, e, bw, depth+2, threads, bar)
+			err = m.dumpDir(s, e.Attr.Inode, e, bw, depth+2, threads, showProgress)
 		} else {
 			err = e.writeJSON(bw, depth+2)
 		}
@@ -3628,15 +3634,15 @@ func (m *dbMeta) dumpDir(s *xorm.Session, inode Ino, tree *DumpedEntry, bw *bufi
 		if i != len(entries)-1 {
 			bwWrite(",")
 		}
-		if bar != nil {
-			bar.Increment()
+		if showProgress != nil {
+			showProgress(0, 1)
 		}
 	}
 	bwWrite(fmt.Sprintf("\n%s}\n%s}", strings.Repeat(jsonIndent, depth+1), strings.Repeat(jsonIndent, depth)))
 	return nil
 }
 
-func (m *dbMeta) dumpDirFast(inode Ino, tree *DumpedEntry, bw *bufio.Writer, depth int, bar *utils.Bar) error {
+func (m *dbMeta) dumpDirFast(inode Ino, tree *DumpedEntry, bw *bufio.Writer, depth int, showProgress func(totalIncr, currentIncr int64)) error {
 	bwWrite := func(s string) {
 		if _, err := bw.WriteString(s); err != nil {
 			panic(err)
@@ -3655,15 +3661,15 @@ func (m *dbMeta) dumpDirFast(inode Ino, tree *DumpedEntry, bw *bufio.Writer, dep
 
 		entry.Name = string(e.Name)
 		if e.Type == TypeDirectory {
-			_ = m.dumpDirFast(e.Inode, entry, bw, depth+2, bar)
+			_ = m.dumpDirFast(e.Inode, entry, bw, depth+2, showProgress)
 		} else {
 			_ = entry.writeJSON(bw, depth+2)
 		}
 		if i != len(edges)-1 {
 			bwWrite(",")
 		}
-		if bar != nil {
-			bar.Increment()
+		if showProgress != nil {
+			showProgress(0, 1)
 		}
 	}
 	bwWrite(fmt.Sprintf("\n%s}\n%s}", strings.Repeat(jsonIndent, depth+1), strings.Repeat(jsonIndent, depth)))
@@ -3778,7 +3784,7 @@ func (m *dbMeta) DumpMeta(w io.Writer, root Ino, threads int, keepSecret, fast, 
 					Type:  typeToString(TypeDirectory),
 				},
 			}
-			if err = m.dumpEntry(s, root, TypeDirectory, tree); err != nil {
+			if err = m.dumpEntry(s, root, TypeDirectory, tree, nil); err != nil {
 				return err
 			}
 			if root == 1 && !skipTrash {
@@ -3789,7 +3795,7 @@ func (m *dbMeta) DumpMeta(w io.Writer, root Ino, threads int, keepSecret, fast, 
 						Type:  typeToString(TypeDirectory),
 					},
 				}
-				if err = m.dumpEntry(s, TrashInode, TypeDirectory, trash); err != nil {
+				if err = m.dumpEntry(s, TrashInode, TypeDirectory, trash, nil); err != nil {
 					return err
 				}
 			}
@@ -3879,15 +3885,15 @@ func (m *dbMeta) DumpMeta(w io.Writer, root Ino, threads int, keepSecret, fast, 
 			bar.IncrTotal(1)
 			bar.Increment()
 		}
-		totalBean := &counter{Name: "totalInodes"}
-		if _, err := s.Get(totalBean); err != nil {
-			return err
+		showProgress := func(totalIncr, currentIncr int64) {
+			bar.IncrTotal(totalIncr)
+			bar.IncrInt64(currentIncr)
 		}
-		bar.SetTotal(totalBean.Value)
 		if m.snap != nil {
-			_ = m.dumpDirFast(root, tree, bw, 1, bar)
+			_ = m.dumpDirFast(root, tree, bw, 1, showProgress)
 		} else {
-			if err = m.dumpDir(s, root, tree, bw, 1, threads, bar); err != nil {
+			showProgress(int64(len(tree.Entries)), 0)
+			if err = m.dumpDir(s, root, tree, bw, 1, threads, showProgress); err != nil {
 				logger.Errorf("dump dir %d failed: %s", root, err)
 				return fmt.Errorf("dump dir %d failed", root) // don't retry
 			}
@@ -3897,9 +3903,10 @@ func (m *dbMeta) DumpMeta(w io.Writer, root Ino, threads int, keepSecret, fast, 
 				return err
 			}
 			if m.snap != nil {
-				_ = m.dumpDirFast(TrashInode, trash, bw, 1, bar)
+				_ = m.dumpDirFast(TrashInode, trash, bw, 1, showProgress)
 			} else {
-				if err = m.dumpDir(s, TrashInode, trash, bw, 1, threads, bar); err != nil {
+				showProgress(int64(len(trash.Entries)), 0)
+				if err = m.dumpDir(s, TrashInode, trash, bw, 1, threads, showProgress); err != nil {
 					logger.Errorf("dump trash failed: %s", err)
 					return fmt.Errorf("dump trash failed") // don't retry
 				}
