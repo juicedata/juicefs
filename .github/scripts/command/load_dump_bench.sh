ccff8302bc662abed7d1071626cde26667d8d0ba
zhoucheng <zhoucheng@juicedata.io>
2025-01-05 17:39:49 +1000

    load dump test

--- .github/scripts/command/load_dump_bench.sh
+++ .github/scripts/command/load_dump_bench.sh
@@ -11,31 +11,32 @@ if [ "$START_META" = true ]; then
 fi
 
 test_load_dump_with_small_dir(){
+  do_load_dump_with_small_dir
+}
+
+test_load_dump_with_small_dir_in_binary(){
+  do_load_dump_with_small_dir --binary
+}
+
+do_load_dump_with_small_dir(){
+  option=$1
   prepare_test
   create_database $META_URL
-  echo meta_url is: $META_URL
-  wget -q https://s.juicefs.com/static/bench/2M_emtpy_files.dump.gz
-  gzip -dfk  2M_emtpy_files.dump.gz
-  load_file=2M_emtpy_files.dump
-  start=`date +%s`
-  ./juicefs load $META_URL $load_file
-  end=`date +%s`
-  runtime=$((end-start))
-  version=$(./juicefs -V|cut -b 17- | sed 's/:/-/g')
-  .github/scripts/save_benchmark.sh --name load_small_dir --result $runtime --meta $META --storage file
-  echo "load cost $runtime seconds"
-  start=`date +%s`
-  ./juicefs dump $META_URL dump.json --fast
-  end=`date +%s`
-  runtime=$((end-start))
-  echo "dump cost $runtime seconds"
-  .github/scripts/save_benchmark.sh --name dump_small_dir --result $runtime --meta $META --storage file
-  ./juicefs mount $META_URL /jfs -d --no-usage-report
-  inode=$(df -i /jfs | grep JuiceFS |awk -F" " '{print $3}')
-  if [ "$inode" -ne "2233313" ]; then 
-    echo "<FATAL>: inode error: $inode"
-    exit 1
+  ./juicefs format $META_URL myjfs
+  ./juicefs mount -d $META_URL /tmp/jfs
+  ./juicefs mdtest /mdtest --depth=3 --dirs=10 --files=10 --threads=100 --write=8192
+  df -i /tmp/jfs
+  ./juicefs dump $META_URL dump.json $option --threads=50
+  umount_jfs /tmp/jfs $META_URL
+  python3 .github/scripts/flush_meta.py $META_URL
+  create_database $META_URL
+  if [[ "$options" == *"--binary"* ]]; then
+    ./juicefs load dump.json $META_URL --binary
+  else
+    ./juicefs load dump.json $META_URL
   fi
+  ./juicefs mount $META_URL /tmp/jfs -d
+  df -i /tmp/jfs
 }
 
 test_load_dump_with_big_dir_subdir(){
