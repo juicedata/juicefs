14373d55cc8fce0067a873a93ef27720e23b44a6
zhoucheng <zhoucheng@juicedata.io>
2025-02-05 16:03:42 +1000

    wip

--- .github/scripts/cache.sh
+++ .github/scripts/cache.sh
@@ -161,7 +161,7 @@ test_cache_large_write(){
     ratio=$(get_warmup_ratio)
     [[ "$ratio" = 0 ]] || (echo "ratio($ratio) should less than 0" && exit 1)
     ./juicefs mount $META_URL /tmp/jfs -d --cache-large-write -v
-    ps -ef | grep juicefs
+    # ps -ef | grep juicefs
     dd if=/dev/zero of=/tmp/jfs/test1 bs=1M count=200
     ./juicefs warmup /tmp/jfs/test1 --check 2>&1 | tee warmup.log
     # TODO: should check the ratio
