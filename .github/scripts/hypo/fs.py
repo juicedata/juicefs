7832c6c37e2c5885fb9eeb2381a47596213be36d
ZhouCheng <zhoucheng@juicedata.io>
2025-04-18 13:09:28 +0800

    fix seed

--- .github/scripts/hypo/fs.py
+++ .github/scripts/hypo/fs.py
@@ -101,7 +101,8 @@ class JuicefsMachine(RuleBasedStateMachine):
             return False
         # TODO: ignore the diff temp, we should check the difference of result1 and result2 in the future.
         # Ref: https://github.com/juicedata/juicefs/issues/5982
-        if self.use_sdk and isinstance(result1, Exception) and isinstance(result2, Exception):
+        ignore_diff_errors = os.environ.get('IGNORE_DIFF_ERRORS', 'false').lower() == 'true'
+        if ignore_diff_errors and isinstance(result1, Exception) and isinstance(result2, Exception):
             return True
         if isinstance(result1, Exception):
             if 'panic:' in str(result1) or 'panic:' in str(result2):
