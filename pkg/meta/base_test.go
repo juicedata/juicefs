96f037ddeded06a431ca572cbfd85f0662bfef2e
jiefenghuang <jiefeng@juicedata.io>
2025-04-25 14:09:43 +0800

    fix
    Signed-off-by: jiefenghuang <jiefeng@juicedata.io>

--- pkg/meta/base_test.go
+++ pkg/meta/base_test.go
@@ -3060,11 +3060,10 @@ func testClone(t *testing.T, m Meta) {
 		})
 
 	}
-	m.getBase().stopDeleteSliceTasks()
+	time.Sleep(1 * time.Second)
 	if !sli1del || !sli2del {
 		t.Fatalf("slice should be deleted")
 	}
-	m.getBase().startDeleteSliceTasks()
 	nodes := m.(engine).doFindDetachedNodes(time.Now())
 	if len(nodes) != 4 {
 		t.Fatalf("find detached nodes error: %v", nodes)
