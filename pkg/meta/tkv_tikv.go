ab60b73b5724e5f0f4f2e46c17b8012721663b56
hang01.li <hang01.li@hobot.ai>
2025-05-30 18:49:27 +0800

    tikv: support  and  options

--- pkg/meta/tkv_tikv.go
+++ pkg/meta/tkv_tikv.go
@@ -92,13 +92,19 @@ func newTikvClient(addr string) (tkvClient, error) {
 	}
 
 	if strings.ToLower(query.Get("open-tso-follower-proxy")) == "true" {
-		logger.Infof("Enabling TSO Follower Proxy")
-		client.KVStore.GetPDClient().UpdateOption(pd.EnableTSOFollowerProxy, true)
+		if err := client.KVStore.GetPDClient().UpdateOption(pd.EnableTSOFollowerProxy, true); err != nil {
+			logger.Warnf("Failed to enable TSO Follower Proxy: %v", err)
+		} else {
+			logger.Infof("Enabling TSO Follower Proxy")
+		}
 
 		if waitStr := query.Get("max-tso-batch-wait-interval"); waitStr != "" {
 			if waitDur, err := time.ParseDuration(waitStr); err == nil {
-				client.KVStore.GetPDClient().UpdateOption(pd.MaxTSOBatchWaitInterval, waitDur)
-				logger.Infof("Set MaxTSOBatchWaitInterval to %s", waitDur)
+				if err := client.KVStore.GetPDClient().UpdateOption(pd.MaxTSOBatchWaitInterval, waitDur); err != nil {
+					logger.Warnf("Failed to set MaxTSOBatchWaitInterval: %v", err)
+				} else {
+					logger.Infof("Set MaxTSOBatchWaitInterval to %s", waitDur)
+				}
 			} else {
 				logger.Warnf("Failed to parse max-tso-batch-wait-interval (%s): %v", waitStr, err)
 			}
