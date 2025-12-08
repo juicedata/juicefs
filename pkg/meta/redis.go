5e91b9201beb26eb9ec7510cc8f389f9c5351a81
Frice Xu <39143307+Xuyuchao-juice@users.noreply.github.com>
2025-12-08 14:24:51 +0800

    meta/sql: optimizate the rmr emptyDir (#6457)
    Signed-off-by: jiefenghuang <jiefeng@juicedata.io>
    Co-authored-by: jiefenghuang <jiefeng@juicedata.io>

--- pkg/meta/redis.go
+++ pkg/meta/redis.go
@@ -1721,6 +1721,10 @@ func (m *redisMeta) doUnlink(ctx Context, parent Ino, name string, attr *Attr, s
 	return errno(err)
 }
 
+func (m *redisMeta) doBatchUnlink(ctx Context, parent Ino, entries []Entry, length *int64, space *int64, inodes *int64, userGroupQuotas *[]userGroupQuotaDelta, skipCheckTrash ...bool) syscall.Errno {
+	return syscall.ENOTSUP
+}
+
 func (m *redisMeta) doRmdir(ctx Context, parent Ino, name string, pinode *Ino, oldAttr *Attr, skipCheckTrash ...bool) syscall.Errno {
 	var trash Ino
 	if !(len(skipCheckTrash) == 1 && skipCheckTrash[0]) {
