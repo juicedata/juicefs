07ec0d01b075d0f47c4b19987e5caf0921575a55
Eitan Akman <eitan.akman@datadoghq.com>
2025-12-10 08:58:48 -0500

    commit from Wed Dec 10 08:58:48 EST 2025

--- pkg/meta/sql.go
+++ pkg/meta/sql.go
@@ -5191,7 +5191,7 @@ func (m *dbMeta) cloneTree(ctx Context, srcIno Ino, parent Ino, name string, dst
 	}
 
 	// Process nodes in batches - increased for better performance
-	const batchSize = 2000
+	const batchSize = 1000
 	for i := 0; i < len(allNodes); i += batchSize {
 		end := i + batchSize
 		if end > len(allNodes) {
