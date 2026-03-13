764100d87895468836ee4de356e265b48e53a3e9
ZhouCheng <zhoucheng@juicedata.io>
2026-03-13 15:46:23 +0800

    bugfix

--- .github/scripts/perf/test_cases/mdtest_list.sh
+++ .github/scripts/perf/test_cases/mdtest_list.sh
@@ -6,12 +6,12 @@ COMPARISON_MODE="lower_is_better"
 
 prepare() {
     prepare0 $@
-    ./juicefs mdtest $(get_meta_url $META) /mdtest --depth 0 --dirs 1 --files 10000 --threads 100
+    ./juicefs mdtest $(get_meta_url $META) /mdtest --depth 0 --dirs 1 --files 5000 --threads 100
     echo 3 > /proc/sys/vm/drop_caches
 }
 
 run_test() {
-    { time ls "/tmp/jfs/mdtest/mdtest_tree.0/" > /dev/null 2>&1; } 2>&1
+    { time ls /tmp/jfs/mdtest/test-dir.0-0/mdtest_tree.0/ > /dev/null 2>&1; } 2>&1
 }
 
 parse_result() {
