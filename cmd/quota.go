53907fb31b5f1513b49b05dab226b87ed4e54891
xuyuchao <xuyuchao@juicedata.io>
2026-03-13 10:35:25 +0800

    fix compile

--- cmd/quota.go
+++ cmd/quota.go
@@ -212,7 +212,8 @@ func quota(c *cli.Context) error {
  			if repair {
  				action = "check and repair"
  			}
- 			fmt.Printf("Warning: No --path specified. This will scan the entire filesystem to %s userQuotas and groupQuotas.\n", action)			fmt.Print("Do you want to continue? [y/N]: ")
+ 			fmt.Printf("Warning: No --path specified. This will scan the entire filesystem to %s userQuotas and groupQuotas.\n", action)			
+			fmt.Print("Do you want to continue? [y/N]: ")
 			reader := bufio.NewReader(os.Stdin)
 			response, _ := reader.ReadString('\n')
 			response = strings.TrimSpace(strings.ToLower(response))
