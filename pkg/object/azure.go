47c7aaa60e94e685ce6d556ccd883a10e412b96d
zhijian <djx@juicedata.io>
2026-03-31 17:22:56 +0800

    support azure

--- pkg/object/azure.go
+++ pkg/object/azure.go
@@ -114,7 +114,20 @@ func (b *wasb) Put(ctx context.Context, key string, data io.Reader, getters ...A
 }
 
 func (b *wasb) Copy(ctx context.Context, dst, src string) error {
-	// fixme: copy should support change object tier
+	// If a tier ID is provided in the context and the source and destination are the same,
+	// we interpret this as a request to change the storage class (tier) of the existing blob without copying.
+	// In this case, we call SetTier on the blob client instead of performing a copy operation.
+	if id, ok := ctx.Value(TierKey{}).(uint8); ok && src == dst {
+		blobClient := b.azblobCli.ServiceClient().NewContainerClient(b.cName).NewBlobClient(src)
+		if t, ok := b.tiers[id]; ok {
+			if _, err := blobClient.SetTier(ctx, *str2Tier(t.Sc), &blob2.SetTierOptions{}); err != nil {
+				return err
+			}
+		} else {
+			return fmt.Errorf("invalid tier id: %d", id)
+		}
+		return nil
+	}
 	dstCli := b.container.NewBlobClient(dst)
 	srcCli := b.container.NewBlobClient(src)
 	options := &blob2.CopyFromURLOptions{}
@@ -201,6 +214,7 @@ func (b *wasb) SetStorageClass(sc string) error {
 
 // Restore Azure does not support restoring to a temporary read-only state; it can only directly permanently change the tier.
 func (b *wasb) Restore(ctx context.Context, key string) error {
+	logger.Infof("Azure does not support restoring to a temporary read-only state; it can only directly permanently change the tier.")
 	return notSupported
 }
 
