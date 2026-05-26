9db72f13f513e73c15377bb0029bc4dfe6ebc02d
Xuhui zhang <xuhui@juicedata.io>
2026-05-26 18:23:41 +0800

    pkg/sync: fix checkpoint file placement for file and dir sync
    Signed-off-by: Xuhui zhang <xuhui@juicedata.io>

--- pkg/sync/sync.go
+++ pkg/sync/sync.go
@@ -903,10 +903,9 @@ func doCopyMultiple(src, dst object.ObjectStorage, key string, size int64, mtime
 			parts[num], chksum, copyErr = doCopyRange(src, dst, key, int64(num)*partSize, sz, upload, num, abort, calChksum)
 			chksums[num] = chksumWithSz{chksum, sz}
 			if copyErr == nil {
+				copiedBytes.IncrInt64(sz)
 				if state != nil {
 					uploads.MarkMultipartPart(key, state, parts[num], chksum, calChksum)
-				} else {
-					copiedBytes.IncrInt64(sz)
 				}
 			}
 			errs <- copyErr
