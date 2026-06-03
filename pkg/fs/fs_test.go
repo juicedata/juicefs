fa0a5c6f36badc3f47fe00928cfed959f8ff487a
jiefenghuang <jiefeng@juicedata.io>
2026-06-03 12:20:32 +0800

    pkg/fs: replace hardcoded temp paths with t.TempDir() in tests

--- pkg/fs/fs_test.go
+++ pkg/fs/fs_test.go
@@ -19,6 +19,7 @@ package fs
 import (
 	"io"
 	"os"
+	"path/filepath"
 	"sort"
 	"syscall"
 	"testing"
@@ -292,7 +293,7 @@ func createTestFS(t *testing.T) *FileSystem {
 		DirEntryTimeout: time.Millisecond * 100,
 		EntryTimeout:    time.Millisecond * 100,
 		AttrTimeout:     time.Millisecond * 100,
-		AccessLog:       "/tmp/juicefs.access.log",
+		AccessLog:       filepath.Join(t.TempDir(), "juicefs.access.log"),
 	}
 	objStore, _ := object.CreateStorage("mem", "", "", "", "")
 	store := chunk.NewCachedStore(objStore, *conf.Chunk, nil)
