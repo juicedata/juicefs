cae068177cdb465792e62b440aca47f26dcad221
zhijian <djx@juicedata.io>
2026-06-03 21:48:06 +0800

    fix

--- pkg/meta/load_dump_test.go
+++ pkg/meta/load_dump_test.go
@@ -370,7 +370,7 @@ func testLoadDump(t *testing.T, name, addr string) {
 
 func TestLoadDump(t *testing.T) { //skip mutate
 	testLoadDump(t, "redis", "redis://127.0.0.1/10")
-	testLoadDump(t, "sqlite3", "sqlite:///tmp/sqlite3.db")
+	testLoadDump(t, "sqlite3", "sqlite3:///tmp/sqlite3.db")
 	testLoadDump(t, "badger", "badger://jfs-load-dump")
 	testLoadDump(t, "tikv", "tikv://127.0.0.1:2379/jfs-load-dump")
 }
