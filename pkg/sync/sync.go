f7cef2a8be33487acbc0a9e91de5ace4d3cc75aa
jiefenghuang <jiefeng@juicedata.io>
2026-06-04 18:49:12 +0800

    cmd/sync: enhance symlink handling with improved error logging and retry logic (#7030) (#7121)
    Co-authored-by: zhijian <djx@juicedata.io>

--- pkg/sync/sync.go
+++ pkg/sync/sync.go
@@ -1124,7 +1124,7 @@ func worker(tasks chan object.Object, src, dst object.ObjectStorage, config *Con
 
 			if config.Links && obj.IsSymlink() {
 				if err = copyLink(src, dst, key); err != nil {
-					logger.Errorf("copy link failed: %s", err)
+					logger.Errorf("copy link %s failed: %s", key, err)
 				}
 			} else {
 				srcChksum, err = copyData(src, dst, key, obj.Size(), obj.Mtime(), config.CheckAll || config.CheckNew, uploads)
@@ -1212,16 +1212,30 @@ func checkChange(src, dst object.ObjectStorage, obj object.Object, key string, c
 }
 
 func copyLink(src object.ObjectStorage, dst object.ObjectStorage, key string) error {
-	if p, err := src.(object.SupportSymlink).Readlink(key); err != nil {
+	var p string
+	var err error
+	if p, err = src.(object.SupportSymlink).Readlink(key); err != nil {
 		return err
-	} else {
-		if err := dst.Delete(ctx, key); err != nil {
-			logger.Debugf("Deleted %s from %s ", key, dst)
-			return err
-		}
-		// TODO: use relative path based on option
-		return dst.(object.SupportSymlink).Symlink(p, key)
 	}
+	return try(3, func() (err error) {
+		// TODO: use relative path based on option
+		if err := dst.(object.SupportSymlink).Symlink(p, key); err != nil {
+			if info, err := dst.Head(ctx, key); err == nil && info.IsSymlink() {
+				if cPath, err2 := dst.(object.SupportSymlink).Readlink(key); err2 == nil && p == cPath {
+					return nil
+				}
+			}
+			if err := dst.Delete(ctx, key); err != nil {
+				logger.Errorf("delete %s from %s failed: %s", key, dst, err)
+				return err
+			}
+			if err := dst.(object.SupportSymlink).Symlink(p, key); err != nil {
+				logger.Warnf("symlink %s to %s error %s", p, key, err)
+				return err
+			}
+		}
+		return nil
+	})
 }
 
 type objWithSize struct {
