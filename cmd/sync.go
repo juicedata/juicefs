e0147851400d16a9127bc2a64f3ead01cbab5cca
zhijian <djx@juicedata.io>
2026-06-22 10:57:15 +0800

    cmd/sync: avoid print the secret key in the log

--- cmd/sync.go
+++ cmd/sync.go
@@ -398,7 +398,7 @@ func createSyncStorage(uri string, conf *sync.Config) (object.ObjectStorage, err
 	uri, token := extractToken(uri)
 	u, err := url.Parse(uri)
 	if err != nil {
-		logger.Fatalf("Can't parse %q: %s", utils.RemovePassword(uri), err.Error())
+		logger.Fatalf("Can't parse %q: %s", utils.RemovePassword(uri), utils.RemovePassword(err.Error()))
 	}
 	user := u.User
 	var accessKey, secretKey string
