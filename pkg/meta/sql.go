8f7421368bcd28a903aedaf9160c8542f199f3fb
Xuhui zhang <xuhui@juicedata.io>
2026-09-18 16:09:07 +0800

    meta: include rdev in CREATE changelogs

--- pkg/meta/sql.go
+++ pkg/meta/sql.go
@@ -1977,7 +1977,7 @@ func (m *dbMeta) doMknod(ctx Context, parent Ino, name string, _type uint8, mode
 		if behavior == nil {
 			behavior = runtime.GOOS
 		}
-		m.genLog(ctx, s, now, "CREATE(%d,%s,%d,%d,%d,%d,%d,%s,%s,%t):%d", parent, logEncode2(name), ctx.Uid(), ctx.Gid(), _type, mode, cumask, logEncode2(path), behavior, updateParent, *inode)
+		m.genLog(ctx, s, now, "CREATE(%d,%s,%d,%d,%d,%d,%d,%s,%s,%t,%d):%d", parent, logEncode2(name), ctx.Uid(), ctx.Gid(), _type, mode, cumask, logEncode2(path), behavior, updateParent, attr.Rdev, *inode)
 		return nil
 	}))
 }
